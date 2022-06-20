62280b20be2d29c5091dd93855d4b99e1be37d0e
Adam Kroon <adam.kroon@gov.bc.ca>
2022-06-20 10:52:36 -0700

    adding better api structure

---
+++ src/backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const API_PORT = process.env.API_PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(API_PORT, () => console.log(`Server started and is listening on port ${API_PORT}`));
