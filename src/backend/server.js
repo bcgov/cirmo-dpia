baca74df521bb29a75bfe8dc9eb30b495ca3f623
Adam Kroon <adam.kroon@gov.bc.ca>
2022-06-27 13:10:54 -0700

    adding code coverage

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,10 +4,6 @@ const { clear } = require('winston');
 const app = express();
 const API_PORT = process.env.API_PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello, world!!');
-});
-
 app.get('/api/v1/health', (req, res) => {
     res.status(200).send('Application is healthy!');
     console.log('GET to Application Health');
