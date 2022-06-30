d1d951b839e24424ecfad2d92ce67f466a307af6
Adam Kroon <adam.kroon@gov.bc.ca>
2022-06-30 11:09:46 -0700

    adding component specific directory to deploy script

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -6,7 +6,7 @@ objects=( $API_DC_TEMPLATE $API_SERVICE_TEMPLATE $API_ROUTE_TEMPLATE )
 
 for i in "${objects[@]}"
 do
-    oc process -f /home/runner/work/cirmo-dpia/cirmo-dpia/openshift/templates/api/"$i" --namespace=$NAMESPACE \
+    oc process -f /home/runner/work/cirmo-dpia/cirmo-dpia/openshift/templates/$DIR/"$i" --namespace=$NAMESPACE \
         -p APPLICATION_NAME=$APPLICATION_NAME \
         -p LICENSE_PLATE=$LICENSE_PLATE \
         -p ENVIRONMENT=$ENVIRONMENT \
