ecbe47f36e871eb703620f22ec4c8acaaebcb881
Adam Kroon <adam.kroon@gov.bc.ca>
2022-07-07 09:55:14 -0700

    adding prod route and service templates, changing application health script

--- .github/helpers/application-health.sh
+++ .github/helpers/application-health.sh
@@ -2,4 +2,9 @@
 
 # script to watch deployment of deployment config in a given namespace to ensure it is healthy and works
 
-oc rollout status -n $NAMESPACE dc/$APPLICATION_NAME-${IMAGE_TAG} --watch
+if [-z "$IMAGE_TAG"]
+then
+    oc rollout status -n $NAMESPACE dc/$APPLICATION_NAME --watch
+else
+    oc rollout status -n $NAMESPACE dc/$APPLICATION_NAME-$IMAGE_TAG --watch
+fi
