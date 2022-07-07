ba731c3e9f4f88cb94dbad55274e0fb4861c684c
Adam Kroon <adam.kroon@gov.bc.ca>
2022-07-07 11:08:11 -0700

    space typo in bash script

--- .github/helpers/application-health.sh
+++ .github/helpers/application-health.sh
@@ -2,7 +2,7 @@
 
 # script to watch deployment of deployment config in a given namespace to ensure it is healthy and works
 
-if [-z "$IMAGE_TAG"]
+if [ -z "$IMAGE_TAG" ]
 then
     oc rollout status -n $NAMESPACE dc/$APPLICATION_NAME --watch
 else
