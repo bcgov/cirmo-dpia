3db92949741a10c23bfa4979f3523282429264c4
Adam Kroon <adam.kroon@gov.bc.ca>
2022-08-10 16:22:59 -0700

    adding localhost as ip

--- src/backend/server.js
+++ src/backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 // Environment Configuration
 require('dotenv').config();
 const API_PORT = process.env.API_PORT || 3000;
-const API_IP_ADDRESS = process.env.API_IP_ADDRESS || '127.0.0.1';
+const API_IP_ADDRESS = process.env.API_IP_ADDRESS || 'localhost';
 const API_VERSION = process.env.API_VERSION || 'v1';
 
 // Route Imports
@@ -23,6 +23,5 @@ app.use(
     swaggerUI.setup(swaggerConf.specs, swaggerConf.uiOptions)
 );
 
-app.get('/api/v1/health', healthCheckRouter);
-
-module.exports = app.listen(API_PORT, API_IP_ADDRESS, () => console.log(`Server version is ${API_VERSION} and is being hosted on ${API_IP_ADDRESS}, listening on port ${API_PORT}`));
+app.get('/api/v1/health', healthCheckRouter)
+module.exports = app.listen(API_PORT, "localhost", () => console.log(`Server version is ${API_VERSION} and is being hosted on ${API_IP_ADDRESS}, listening on port ${API_PORT}`));
