e3d74bdb6353e399c89c58e5c3c133ec231524e7
Adam Kroon <adam.kroon@gov.bc.ca>
2022-08-23 10:43:31 -0700

    cleanup of application health action job and steps

--- .github/helpers/application-health.sh
+++ .github/helpers/application-health.sh
@@ -2,9 +2,4 @@
 
 # script to watch deployment of deployment config in a given namespace to ensure it is healthy and works
 
-if [ -z "$IMAGE_TAG" ]
-then
-    oc rollout status -n $NAMESPACE dc/$APPLICATION_NAME --watch
-else
-    oc rollout status -n $NAMESPACE dc/$APPLICATION_NAME-$IMAGE_TAG --watch
-fi
+oc rollout status -n $NAMESPACE dc/$APPLICATION_NAME --watch
