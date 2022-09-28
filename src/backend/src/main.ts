02c778ed18e340fed70dde75c4124beef7c66c21
Kushal Arora <kushal.arora1@ca.ey.com>
2022-09-28 11:37:38 -0700

    Updated node to 18.9 + Nest initialize with Express app + Swagger

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import 'dotenv/config';
+
+import { createNestApp } from './app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Initialize the Express based Nest application
+  const { app } = await createNestApp();
+  await app.init();
+
+  // App listen to a port
+  await app.listen(process.env.PORT);
 }
 bootstrap();
