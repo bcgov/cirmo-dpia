bc33b769b9fec684ca70193142d27aca6dbe8d88
Bottle7 <brandonjbouchard@gmail.com>
2022-09-29 11:13:15 -0700

    Adds horizontal BC Gov Logo to the frontend public directory and adds it to the Header component.

--- src/frontend/src/components/common/Header.tsx
+++ src/frontend/src/components/common/Header.tsx
@@ -3,8 +3,15 @@ import React from 'react'
 function Header() {
   return (
     <header>
-        <h1>Digital Privacy Impact Assessment (DPIA)<span>beta</span></h1>
-        <button>Log in with IDIR</button>
+        <div className="banner">
+            <a href="https://gov.bc.ca">
+                <img className='logo' src="src/frontend/public/BC_Logo_Horizontal.svg" alt="Go to the Government of British Columbia website" />
+            </a>
+        </div>
+        <div className="other">
+            <h1>Digital Privacy Impact Assessment (DPIA)<span>beta</span></h1>
+            <button>Log in with IDIR</button>
+        </div>
     </header>
   )
 }
