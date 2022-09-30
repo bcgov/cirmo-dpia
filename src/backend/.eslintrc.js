1efa58207e22fac40f9259ea9f4f5ba63d121b29
bing gao <bing@freshworks.io>
2022-09-29 19:52:11 -0700

    remove unuse lint config

--- src/backend/.eslintrc.js
+++ src/backend/.eslintrc.js
@@ -26,19 +26,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
-  "settings": {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
-    },
-    "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-
-        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
-        // use <root>/path/to/folder/tsconfig.json
-        "project": "./tsconfig.json"
-      }
   }
-}
 }
