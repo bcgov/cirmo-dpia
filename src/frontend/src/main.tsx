cf10577bdfce8a4fa1b6b4016f53c674386eefc5
bing gao <bing@freshworks.io>
2022-10-11 19:41:44 -0700

    add ppq landing page

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter,
+  Routes,
+  Route } from "react-router-dom"
 import App from './App'
+import PPQLandingPage from './pages/PPQPage'
 import './sass/index.scss'
 import './sass/common.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+ <BrowserRouter>
+ <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="ppq" element={<PPQLandingPage />} />
+   
+    </Routes>
+ 
+ </BrowserRouter>
+    
   </React.StrictMode>
 )
---
+++ src/frontend/src/pages/PPQPage.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+import ppqImg from '../assets/ppq_homepage.svg'
+
+import Header from '../components/common/Header'
+import Footer from '../components/common/Footer'
+
+
+
+function PPQLandingPage() {
+    return (
+        <div> 
+            <Header/>
+           <section className='ppq-nav-bar'>
+            <a href='/'> <p> Home </p></a>
+            
+            <a href='/ppq'><p> PIA Pathway Questionnaire</p></a>
+           </section>
+            <section className='ppq-section'>
+              
+                <div className='what-is-a-ppq'>
+                <h2 className='ppq-header' >PIA Pathway Questionnaire</h2>
+                    <h2 >what is it?</h2>
+                    <p>The PIA Pathways Questionnaire asks for some basic information about the initiative you're assessing in the PIA. Your answers will help us estimate </p>
+                    <h2>Value of the PPQ</h2> 
+                    <p>The relative complexity of your PIA </p>
+                    <p>	Which PIA template you should fill out</p>
+                    <p>Where to go for help with your PIA</p>
+
+                    <div className="ctas">
+                        <a href="/" className="btn-primary">Get started <FontAwesomeIcon className='icon' icon={faChevronRight} /></a>
+                    </div>
+                    <br/>
+                    <span> <b>Estimated time:</b> 20 minutes</span>
+                </div>
+                <div className="ppq-svg">
+                    <img src={ppqImg} alt="Fill form image" />
+                </div>
+               
+            </section>
+            <Footer/>
+        </div>
+       
+    )
+}
+
+export default PPQLandingPage;
