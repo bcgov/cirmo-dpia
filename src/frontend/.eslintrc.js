90d436539d0a6a5b57e38b8650311bcd397ebbe7
bing gao <bing@freshworks.io>
2022-10-13 09:45:44 -0700

    lint scss

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -1,46 +1,47 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'plugin:import/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jest/recommended',
-        'plugin:prettier/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-        project: './tsconfig.json',
-    },
-    plugins: ['react', '@typescript-eslint', 'jest'],
-    rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'import/no-extraneous-dependencies': [
-            'error',
-            {
-                devDependencies: [
-                    'test.{ts,tsx}', // repos with a single test file
-                    'test-*.{ts,tsx}', // repos with multiple top-level test files
-                    '**/*{.,_}{test,spec,cy}.{ts,tsx}', // tests where the extension or filename suffix denotes that it is a test
-                    '**/jest.config.ts', // jest config
-                    '**/jest.setup.ts', // jest setup
-                    './cypress/support/component.ts',
-                    './cypress.config.ts',
-                ],
-                optionalDependencies: false,
-            },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.scss'],
+  },
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'test.{ts,tsx}', // repos with a single test file
+          'test-*.{ts,tsx}', // repos with multiple top-level test files
+          '**/*{.,_}{test,spec,cy}.{ts,tsx}', // tests where the extension or filename suffix denotes that it is a test
+          '**/jest.config.ts', // jest config
+          '**/jest.setup.ts', // jest setup
+          './cypress/support/component.ts',
+          './cypress.config.ts',
         ],
-    },
-}
+        optionalDependencies: false,
+      },
+    ],
+  },
+};
