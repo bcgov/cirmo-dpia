43e61ce07edbbb3d3ac117f62acbcfc0f26055af
Bottle7 <brandonjbouchard@gmail.com>
2022-10-14 14:00:25 -0700

    Adds tsconfig.eslint.json file and attempts to lint code.

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -5,12 +5,13 @@ import App from './App';
 import PPQLandingPage from './pages/PPQPage';
 import './sass/index.scss';
 import './sass/common.scss';
+import LandingPage from './pages/LandingPage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/ppq" element={<PPQLandingPage />} />
       </Routes>
     </BrowserRouter>
