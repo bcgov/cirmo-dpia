f9d2d45fdf23100e3f1805a630b54200ee58cf5d
bing gao <bing@freshworks.io>
2022-10-18 16:54:46 -0700

    update css and content for ppq page

--- src/frontend/src/pages/PPQPage.tsx
+++ src/frontend/src/pages/PPQPage.tsx
@@ -22,17 +22,16 @@ function PPQLandingPage() {
         <div className='col-md-6'>
           <h2 >PIA Pathway Questionnaire</h2>
           <br />
-          <h2>what is it?</h2>
-          <p>
+          <p className='col-mb-6'>
             The PIA Pathways Questionnaire asks for some basic information about
             the initiative you're assessing in the PIA. Your answers will help
             us estimate{' '}
           </p>
-          <h2>Value of the PPQ</h2>
-          <p>The relative complexity of your PIA </p>
-          <p> Which PIA template you should fill out</p>
-          <p>Where to go for help with your PIA</p>
-
+         <ul className='col-mb-6 list'>
+          <li>The relative complexity of your PIA </li>
+          <li> Which PIA template you should fill out</li>
+          <li>Where to go for help with your PIA</li>
+          </ul> 
           <div data-cy="ppq-btn" className="ctas">
             <Link to="/ppq-form" className="btn-primary">
               Get started{' '}
