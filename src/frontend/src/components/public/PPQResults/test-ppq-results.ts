5929562012fe00c39f8470e478496abd83db2d6c
Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
2022-10-18 10:16:39 -0700

    Update results page copy
    Changes to page copy to reflect update to associated JIRA ticket.

--- src/frontend/src/components/public/PPQResults/test-ppq-results.ts
+++ src/frontend/src/components/public/PPQResults/test-ppq-results.ts
@@ -4,22 +4,22 @@ const ppqTestResults = [
   {
     id: 1,
     icon: faSlidersH,
-    title: "Complexity",
-    text: "Your initiative is likely medium complexity.",
+    title: "Implication",
+    text: "We'll estimate whether your initiative has a relatively low, medium or high level of potential privacy implications.",
     button: false
   },
   {
     id: 2,
     icon: faHandsHelping,
     title: "Collaborators",
-    text: "In drafting your PIA, you may want to connect with:\n\nMinistry Privacy Officer (MPO)\n\n Ministry Information Security Officer (MISO)",
+    text: "We'll estimate how many subject matter experts you may need to collaborate with on you PIA.",
     button: false
   },
   {
     id: 3,
     icon: faFileSignature,
     title: "Template",
-    text: "Your initiative is likely medium complexity.",
+    text: "We'll show you which PIA template to download depending on whether you're doing a regular PIA, an initiative update or a corporate checklist.",
     button: true,
     buttonText: "Download",
     buttonIcon: faFileDownload
