70949aca3131ca6421827486c5e07be33e0e7c68
Bottle7 <brandonjbouchard@gmail.com>
2022-11-02 09:17:29 -0700

    Adds rel='external noreferrer' to MPO contact link for increased security and to satisfy linter.

--- src/frontend/src/components/public/PPQConnect/index.tsx
+++ src/frontend/src/components/public/PPQConnect/index.tsx
@@ -35,7 +35,7 @@ const PPQConnect = (props: IComponentProps) => {
         <h2>1. Find your MPO</h2>
         <p className="mpo-find-contact">
           Every ministry has a Ministry Privacy Officer (MPO).
-          <a href="https://www2.gov.bc.ca/gov/content/governments/services-for-government/information-management-technology/privacy/resources/privacy-officers" target='_blank'>
+          <a href="https://www2.gov.bc.ca/gov/content/governments/services-for-government/information-management-technology/privacy/resources/privacy-officers" rel="external noreferrer" target='_blank'>
             Identify your MPO and their contact information.
           </a>
         </p>
