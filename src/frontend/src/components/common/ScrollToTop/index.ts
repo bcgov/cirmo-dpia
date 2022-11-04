f8672df0e726146ab447d0f7bc670a8ead16c62a
Bottle7 <brandonjbouchard@gmail.com>
2022-11-04 13:22:16 -0700

    Creates ScrollToTop component to handle scrolling to the top of the page on re-route.

---
+++ src/frontend/src/components/common/ScrollToTop/index.ts
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'auto', // Optional if you want to skip the scrolling animation
+    });
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
