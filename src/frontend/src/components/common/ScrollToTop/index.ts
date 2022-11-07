a6d74fdeca9b5b298b91c1146a8ddbc1ee08a21d
Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
2022-11-07 08:19:17 -0800

    Removes comments

--- src/frontend/src/components/common/ScrollToTop/index.ts
+++ src/frontend/src/components/common/ScrollToTop/index.ts
@@ -5,11 +5,10 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
     document.documentElement.scrollTo({
       top: 0,
       left: 0,
-      behavior: 'auto', // Optional if you want to skip the scrolling animation
+      behavior: 'auto',
     });
   }, [pathname]);
 
