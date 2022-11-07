4b7da2f6c56387f8689fedcd0b7631b8d45ea6d8
Bottle7 <brandonjbouchard@gmail.com>
2022-11-07 15:24:12 -0800

    Updates Connect page with new layout and copy.

---
+++ src/frontend/src/components/public/PPQConnect/messages.ts
@@ -0,0 +1,27 @@
+export default {
+  Headings: {
+    Title: {
+      en: `Thank you for answering the PPQ`,
+    },
+    Subtitle: {
+      en: `Your answers will help us streamline the review of this PIA.`,
+    },
+    One: {
+      en: `1. Download your results`,
+    },
+    Two: {
+      en: `2. Connect with PCT`,
+    },
+    Three: {
+      en: `3. Wait`,
+    },
+  },
+  Text: {
+    Two: {
+      en: `Email the PPQ and PIA intake form (if applicable) to Privacy, Compliance and Training (PCT) at [PIA.Intake@gov.bc.ca](mailto:pia.intake@gov.bc.ca)`,
+    },
+    Three: {
+      en: `PCT will send you a file number, and a PIA analyst will contact you with any questions.`,
+    },
+  },
+};
