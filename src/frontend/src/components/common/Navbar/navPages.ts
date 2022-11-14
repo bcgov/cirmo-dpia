4d03ed8aceeb12c618c517223f0116e2ea5d691a
bing gao <bing@freshworks.io>
2022-11-14 15:29:49 -0800

    fix lint

--- src/frontend/src/components/common/Navbar/navPages.ts
+++ src/frontend/src/components/common/Navbar/navPages.ts
@@ -3,11 +3,10 @@ export const NavPages = [
     id: 1,
     label: 'My PIAs',
     link: '/ppq',
-  }, 
+  },
   {
     id: 2,
     label: 'PIA Pathway Questionnaire',
     link: '/ppq',
   },
-
 ];
