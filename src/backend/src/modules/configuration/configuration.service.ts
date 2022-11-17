410f6f2a6fa4b7c441905650c8a3690ca0ad477e
bing gao <bing@freshworks.io>
2022-11-17 11:24:49 -0800

    update env variable to only one

--- src/backend/src/modules/configuration/configuration.service.ts
+++ src/backend/src/modules/configuration/configuration.service.ts
@@ -6,9 +6,8 @@ export class ConfigurationService {
     try {
       // TODO need to check how to get the path of config file
       // hardcode in here or get from a env variables
-      const filePath = process.env.FEATURE_FLAG_FILE_PATH;
-      const fileName = process.env.FEATURE_FLAG_FILE_NAME;
-      const text = fs.readFileSync(filePath + fileName, 'utf8');
+      const filePath = process.env.CONFIG_FILE_PATH;
+      const text = fs.readFileSync(filePath, 'utf8');
       const jsonData = JSON.parse(text);
 
       return jsonData;
