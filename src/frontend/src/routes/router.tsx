262130a793a11ceec8839b4e33f8a0af1042e6c3
bing gao <57013495+binggaofw@users.noreply.github.com>
2022-11-29 13:48:23 -0800

    [Utopia 380] add protected route (#650)
    * add proteced route
    
    * add protected route for app
    
    Co-authored-by: Adam Kroon <Adam.Kroon@gov.bc.ca>

--- src/frontend/src/routes/router.tsx
+++ src/frontend/src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
 import LandingPage from '../pages/LandingPage/LandingPage';
 import PIAIntakeFormPage from '../pages/PIAIntakeForm/PIAIntakeFormPage';
@@ -6,7 +6,20 @@ import PIAIntakeResultsPage from '../pages/PIAIntakeResultsPage';
 import PPQConnectPage from '../pages/PPQConnectPage';
 import PPQFormPage from '../pages/PPQFormPage/PPQFormPage';
 import PPQLandingPage from '../pages/PPQPage/PPQPage';
+import { isAuthenticated } from '../utils/auth';
 import { getConfigFlagFromStorageByName } from '../utils/helper.util';
+interface IComponentProps {
+  isLoggedIn: boolean;
+  redirectPath: string;
+}
+
+const ProtectedRoute = (props: IComponentProps) => {
+  if (!props.isLoggedIn) {
+    return <Navigate to={props.redirectPath} replace />;
+  }
+
+  return <Outlet />;
+};
 
 const Router = () => {
   // will give default value to false if we do not get from the config file due to any technical issue
@@ -17,13 +30,19 @@ const Router = () => {
   return (
     <Routes>
       <Route
-        path="/ppq"
-        element={<PPQLandingPage enablePiaIntakeForm={PIAIntakeFlag} />}
-      />
-      <Route path="/ppq-form" element={<PPQFormPage />} />
-      <Route path="/ppq-connect" element={<PPQConnectPage />} />
-      <Route path="/pia-intake" element={<PIAIntakeFormPage />} />
-      <Route path="/pia-result" element={<PIAIntakeResultsPage />} />
+        element={
+          <ProtectedRoute isLoggedIn={isAuthenticated()} redirectPath={'/'} />
+        }
+      >
+        <Route
+          path="/ppq"
+          element={<PPQLandingPage enablePiaIntakeForm={PIAIntakeFlag} />}
+        />
+        <Route path="/ppq-form" element={<PPQFormPage />} />
+        <Route path="/ppq-connect" element={<PPQConnectPage />} />
+        <Route path="/pia-intake" element={<PIAIntakeFormPage />} />
+        <Route path="/pia-result" element={<PIAIntakeResultsPage />} />
+      </Route>
       <Route path="/" element={<LandingPage />} />
     </Routes>
   );
