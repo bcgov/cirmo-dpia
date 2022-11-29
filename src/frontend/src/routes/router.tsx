bfbd3b8e0e9b754af0f27e6819fa3bd6115a0884
bing gao <57013495+binggaofw@users.noreply.github.com>
2022-11-29 10:42:19 -0800

    [UTOPIA-438]create pia intake result page (#624)
    * create pia intake result page
    
    * remove duplicate scss file for pia intake result page and reuse ppq-connect scss
    
    * update btn css
    
    * update css for pia intake result, add new pia intake result interface and update route config
    
    * rebase with main and remove the back btn for pia-result
    
    * Update src/frontend/src/components/public/PIAIntakeResults/index.tsx
    
    Co-authored-by: Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
    
    Co-authored-by: Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>

--- src/frontend/src/routes/router.tsx
+++ src/frontend/src/routes/router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import LandingPage from '../pages/LandingPage/LandingPage';
 import PIAIntakeFormPage from '../pages/PIAIntakeForm/PIAIntakeFormPage';
+import PIAIntakeResultsPage from '../pages/PIAIntakeResultsPage';
 import PPQConnectPage from '../pages/PPQConnectPage';
 import PPQFormPage from '../pages/PPQFormPage/PPQFormPage';
 import PPQLandingPage from '../pages/PPQPage/PPQPage';
@@ -22,6 +23,7 @@ const Router = () => {
       <Route path="/ppq-form" element={<PPQFormPage />} />
       <Route path="/ppq-connect" element={<PPQConnectPage />} />
       <Route path="/pia-intake" element={<PIAIntakeFormPage />} />
+      <Route path="/pia-result" element={<PIAIntakeResultsPage />} />
       <Route path="/" element={<LandingPage />} />
     </Routes>
   );
