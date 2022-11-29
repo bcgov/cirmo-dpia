c715b45d46608512fb73a633e0ccfc1a23d6f389
Kushal Arora <kushal.arora1@ca.ey.com>
2022-11-28 16:31:08 -0800

    added comments/description to the tests

--- src/backend/test/unit/pia-intake/pia-intake.controller.spec.ts
+++ src/backend/test/unit/pia-intake/pia-intake.controller.spec.ts
@@ -18,6 +18,10 @@ import { PiaIntakeService } from 'src/modules/pia-intake/pia-intake.service';
 import { piaIntakeServiceMock } from 'test/util/mocks/services/pia-intake.service.mock';
 import { UnauthorizedException } from '@nestjs/common';
 
+/**
+ * @Description
+ * This file tests the contents of pia-intake.controller.ts
+ */
 describe('PiaIntakeController', () => {
   let controller: PiaIntakeController;
   let authService: AuthService;
@@ -47,11 +51,38 @@ describe('PiaIntakeController', () => {
     piaIntakeService = module.get<PiaIntakeService>(PiaIntakeService);
   });
 
+  /**
+   * @Description
+   * Dummy test to check if the controller is defined
+   */
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
+  /**
+   * @Description
+   * This test suite validates that the method passes the correct passed values to the service,
+   * mock the service result and return correct result to the user
+   *
+   * @method create
+   */
   describe('`create` method', () => {
+    /**
+     * @Description
+     * This test validates that the user is shown Unauthorized exception (401)
+     * when the access token is not passed by the application.
+     *
+     * As an added check, it also validates that the following methods
+     * authService.getUserInfo, piaIntakeService.create are not being called
+     *
+     * @Input
+     *   - API data mock for pia-intake create form submission
+     *   - No access token is provided
+     *
+     * @Output 401
+     * Unauthorized exception is shown to the user
+     *
+     */
     it('fails when access token is not passed', async () => {
       expect(controller.create(createPiaIntakeMock, null)).rejects.toThrow(
         new UnauthorizedException(),
@@ -61,6 +92,20 @@ describe('PiaIntakeController', () => {
       expect(piaIntakeService.create).not.toHaveBeenCalled();
     });
 
+    /**
+     * @Description
+     * This test validates the happy flow
+     *  - The test calls the following methods if the access token is provided
+     *  - authService.getUserInfo returns the correct user info [mocked]
+     *  - piaIntakeService.create is called with correct mock data
+     *
+     * @Input
+     *   - API data mock for pia-intake create form submission
+     *   - User access token
+     *
+     * @Output 201
+     * Test pass and all methods called with correct data
+     */
     it('succeeds with correct data : Happy flow', async () => {
       const accessToken = accessTokenMock;
       const createPiaIntakeDto: CreatePiaIntakeDto = { ...createPiaIntakeMock };
--- src/backend/test/unit/pia-intake/pia-intake.service.spec.ts
+++ src/backend/test/unit/pia-intake/pia-intake.service.spec.ts
@@ -16,6 +16,10 @@ import { keycloakUserMock } from 'test/util/mocks/data/auth.mock';
 import { KeycloakUser } from 'src/modules/auth/keycloak-user.model';
 import { delay } from 'test/util/testUtils';
 
+/**
+ * @Description
+ * This file tests the contents of pia-intake.service.ts
+ */
 describe('PiaIntakeService', () => {
   let service: PiaIntakeService;
   let piaIntakeRepository;
@@ -37,11 +41,33 @@ describe('PiaIntakeService', () => {
     piaIntakeRepository = module.get(getRepositoryToken(PiaIntakeEntity));
   });
 
+  /**
+   * @Description
+   * Dummy test to check if the service is defined
+   */
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
+  /**
+   * @Description
+   * This test suite validates that the method passes the correct values to the repository,
+   * mocking the database save operation.
+   *
+   * @method create
+   */
   describe('`create` method', () => {
+    /**
+     * This test validates that the it passes the correct data to the repository, saving the pia-intake form.
+     * It also validates that the result received is actually coming from the repository.
+     *
+     * @Input
+     *   - API data mock for pia-intake create form submission
+     *   - User info mock
+     *
+     * @Output
+     *   - an object containing id of the newly created database row
+     */
     it('succeeds calling the database repository with correct data', async () => {
       const createPiaIntakeDto: CreatePiaIntakeDto = { ...createPiaIntakeMock };
       const piaIntakeEntity = { ...piaIntakeEntityMock };
