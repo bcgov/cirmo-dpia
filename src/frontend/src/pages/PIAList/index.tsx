763db4041fbac25e71a6b2a7721d598f2a48e5ff
Bottle7 <brandonjbouchard@gmail.com>
2022-12-05 13:50:50 -0800

    Creates EmptyPIAList component. Temporarily adds a NavLink for access to the PIA list during development.

---
+++ src/frontend/src/pages/PIAList/index.tsx
@@ -0,0 +1,11 @@
+import EmptyPIAList from '../../components/public/EmptyPIAList';
+
+const PIAList = () => {
+  return (
+    <div className="bcgovPageContainer background">
+      <EmptyPIAList />
+    </div>
+  );
+};
+
+export default PIAList;
