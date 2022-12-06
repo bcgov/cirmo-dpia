d7134a7b7478d032756cf0045012c910489f35ed
bing gao <bing@freshworks.io>
2022-12-06 10:39:02 -0800

    read base url from config

--- src/frontend/cypress/e2e/landing-page/landingpage.cy.ts
+++ src/frontend/cypress/e2e/landing-page/landingpage.cy.ts
@@ -3,7 +3,7 @@ describe('Test DPIA Landing Page', () => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
+    // we include qit in our beforeEach function so that it runs before each test
     const url = Cypress.config().baseUrl; //accessing baseUrl
     cy.visit(url);
   });
