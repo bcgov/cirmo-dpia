282fe4b458b7c47686167c4537863b6b3cd7b53d
bing gao <57013495+binggaofw@users.noreply.github.com>
2022-12-07 14:46:23 -0800

    [Utopia 380] fix protected route (#675)
    * re-implement protected route for web
    
    * add new method for protected route
    
    * save temp work
    
    * rollback the implement to original one as we find the issue is not the router code
    
    * rebase with main
    
    * add 404 page not found page
    
    * update check for navbar display
    
    * update not found page css
    
    * change import notfound css file name in common file
    
    * fix lint issue for router.tsx

--- src/frontend/src/routes/router.tsx
+++ src/frontend/src/routes/router.tsx
@@ -1,4 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
+import { Children, useContext, useState } from 'react';
+import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom';
+import NotFound from '../components/common/NotFound';
 
 import LandingPage from '../pages/LandingPage/LandingPage';
 import PIAIntakeFormPage from '../pages/PIAIntakeForm';
@@ -7,7 +9,18 @@ import PIAList from '../pages/PIAListPage';
 import PPQConnectPage from '../pages/PPQConnectPage';
 import PPQFormPage from '../pages/PPQFormPage/PPQFormPage';
 import PPQLandingPage from '../pages/PPQPage/PPQPage';
+import { isAuthenticated } from '../utils/auth';
 import { getConfigFlagFromStorageByName } from '../utils/helper.util';
+
+export const ProtectedRoute = () => {
+  const location = useLocation();
+  const auth = isAuthenticated();
+  return auth ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/" replace state={{ from: location }} />
+  );
+};
 
 const Router = () => {
   // will give default value to false if we do not get from the config file due to any technical issue
@@ -17,16 +30,19 @@ const Router = () => {
   );
   return (
     <Routes>
-      <Route
-        path="/ppq"
-        element={<PPQLandingPage enablePiaIntakeForm={PIAIntakeFlag} />}
-      />
-      <Route path="/ppq-form" element={<PPQFormPage />} />
-      <Route path="/ppq-connect" element={<PPQConnectPage />} />
-      <Route path="/pia-intake" element={<PIAIntakeFormPage />} />
-      <Route path="/pia-result" element={<PIAIntakeResultsPage />} />
-      <Route path="/pia-list" element={<PIAList />} />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/ppq"
+          element={<PPQLandingPage enablePiaIntakeForm={PIAIntakeFlag} />}
+        />
+        <Route path="/ppq-form" element={<PPQFormPage />} />
+        <Route path="/ppq-connect" element={<PPQConnectPage />} />
+        <Route path="/pia-intake" element={<PIAIntakeFormPage />} />
+        <Route path="/pia-result" element={<PIAIntakeResultsPage />} />
+        <Route path="/pia-list" element={<PIAList />} />
+      </Route>
       <Route path="/" element={<LandingPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
