592c2cbafdae1efffb53134a19e5e6844ca36aec
Shivakumar <Anthony.Shivakumar@gov.bc.ca>
2022-12-07 10:19:59 -0800

    [Utopia-511] Update button hover states to match branding guidelines

--- src/frontend/src/components/common/Navbar/index.tsx
+++ src/frontend/src/components/common/Navbar/index.tsx
@@ -18,7 +18,7 @@ function NavBar({ pages }: INavbarPages): ReactElement {
           <ul className="navbar">
             <li key={page.id}>
               <NavLink
-                className="bcgovbtn bcgovbtn__secondary nav-link-button"
+                className="bcgovbtn bcgovbtn__secondary--dark"
                 to={page.link}
               >
                 {page.label}
