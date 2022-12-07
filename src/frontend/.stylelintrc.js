954c70479d2f432ad708e3c23dcb8f6d8a7449df
bing gao <bing@freshworks.io>
2022-12-06 16:38:05 -0800

    add a new regex to check class seletcor name to by pass bem error

--- src/frontend/.stylelintrc.js
+++ src/frontend/.stylelintrc.js
@@ -65,6 +65,8 @@ module.exports = {
         ignoreAtRules: ['else'],
       },
     ],
+    'selector-class-pattern':
+      '^[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$|^.*$',
     'at-rule-no-unknown': [
       // https://stylelint.io/user-guide/rules/list/at-rule-no-unknown/
       true,
