871657b4b02ed661dad661d777d44c22eb041d42
Bottle7 <brandonjbouchard@gmail.com>
2023-01-19 14:05:53 -0800

    Revert "Remove erroneously edited migration file."
    This reverts commit da4c074b1d5962b99b62caf97f5b52745c7b3622.

--- src/backend/src/modules/pia-intake/pia-intake.controller.ts
+++ src/backend/src/modules/pia-intake/pia-intake.controller.ts
@@ -47,6 +47,9 @@ export class PiaIntakeController {
   @ApiCreatedResponse({
     description: 'Successfully submitted a PIA-intake form',
   })
+  @ApiConflictResponse({
+    description: 'Failed to submit the PIA: There is a conflict',
+  })
   async create(
     @Body() createPiaIntakeDto: CreatePiaIntakeDto,
     @Req() req: IRequest,
