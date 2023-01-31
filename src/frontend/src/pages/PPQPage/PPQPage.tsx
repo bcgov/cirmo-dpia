ce7cb9a0570199e7e7f0e08ecf1bb1c93e1e9a01
Shivakumar <Anthony.Shivakumar@gov.bc.ca>
2023-01-31 09:35:18 -0800

    Utopia-688 - Fix breadcrumb alignmnent

--- src/frontend/src/pages/PPQPage/PPQPage.tsx
+++ src/frontend/src/pages/PPQPage/PPQPage.tsx
@@ -16,14 +16,14 @@ interface IComponentProps {
 }
 function PPQLandingPage(props: IComponentProps) {
   const { showMPOContents } = props;
-  return showMPOContents ? (
-    <div className="bcgovPageContainer background">
+  return (
+    <div className="bcgovPageContainer wrapper">
       <Breadcrumbs />
       <div>
         <div>
           <h1 className="mb-4">Create New </h1>
         </div>
-        <div className="container ">
+        <div className="overflow-clip">
           <div className="row mt-2 mb-5 component__row--create-new-form">
             <div className="col ">
               <h2>
@@ -50,69 +50,33 @@ function PPQLandingPage(props: IComponentProps) {
               <img src={piaImg} alt="Fill form image" />
             </div>
           </div>
-
-          <div
-            data-cy="ppq-img"
-            className="row mt-2 mb-5 component__row--create-new-form "
-          >
-            <div className="col">
-              <h2>
-                <b>{messages.PPQHeading.en}</b>
-              </h2>
-              <br />
-              <p>{messages.PPQDescriptionTextInPIAIntake.en}</p>
-              <div data-cy="ppq-btn">
-                <Link to="/ppq-form" className=" bcgovbtn bcgovbtn__primary">
-                  Start PPQ
-                  <FontAwesomeIcon className="icon" icon={faChevronRight} />
-                </Link>
+          {showMPOContents && (
+            <div
+              data-cy="ppq-img"
+              className="row mt-2 mb-5 component__row--create-new-form "
+            >
+              <div className="col">
+                <h2>
+                  <b>{messages.PPQHeading.en}</b>
+                </h2>
+                <br />
+                <p>{messages.PPQDescriptionTextInPIAIntake.en}</p>
+                <div data-cy="ppq-btn">
+                  <Link to="/ppq-form" className=" bcgovbtn bcgovbtn__primary">
+                    Start PPQ
+                    <FontAwesomeIcon className="icon" icon={faChevronRight} />
+                  </Link>
+                </div>
+                <br />
+                <span>
+                  <b>Estimated time:</b> 5 minutes
+                </span>
               </div>
-              <br />
-              <span>
-                <b>Estimated time:</b> 5 minutes
-              </span>
-            </div>
-            <div className="col col-sm-4 rounded float-end">
-              <img src={ppqImg} alt="Fill form image" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="bcgovPageContainer background">
-      <div>
-        <Breadcrumbs />
-        <div>
-          <h1 className="mb-4">Create New </h1>
-        </div>
-        <div className="container ">
-          <div className="row mt-2 mb-5 component__row--create-new-form">
-            <div className="col ">
-              <h2>
-                <b>{messages.PIAIntakeHeading.en}</b>
-              </h2>
-              <br />
-              <p>{messages.PIAIntakeDescriptionText.en}</p>
-
-              <div data-cy="ppq-btn">
-                <Link
-                  to="/pia-intake"
-                  className="bcgovbtn bcgovbtn__primary ppq-btn"
-                >
-                  Start PIA Intake
-                  <FontAwesomeIcon className="icon" icon={faChevronRight} />
-                </Link>
+              <div className="col col-sm-4 rounded float-end">
+                <img src={ppqImg} alt="Fill form image" />
               </div>
-              <br />
-              <span>
-                <b>Estimated time:</b> 20 minutes
-              </span>
             </div>
-            <div className="col col-sm-4 rounded float-end">
-              <img src={piaImg} alt="Fill form image" />
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
