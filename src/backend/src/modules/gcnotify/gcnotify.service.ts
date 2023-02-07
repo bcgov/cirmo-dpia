529c820acbbbd6c720d45b7bae2f94e04d2de13a
Bottle7 <brandonjbouchard@gmail.com>
2023-02-07 11:24:20 -0800

    Adds error handling to the gcnotify service.

--- src/backend/src/modules/gcnotify/gcnotify.service.ts
+++ src/backend/src/modules/gcnotify/gcnotify.service.ts
@@ -1,6 +1,6 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
-import { firstValueFrom } from 'rxjs';
+import { HttpException, Injectable } from '@nestjs/common';
+import { catchError, firstValueFrom, map } from 'rxjs';
 import { GcNotifyEmailDto } from './dto/gcnotify-pia.dto';
 
 @Injectable()
@@ -9,25 +9,33 @@ export class GcNotifyService {
 
   async sendEmail(emailProps: GcNotifyEmailDto): Promise<string> {
     const data = await firstValueFrom(
-      this.httpService.post(
-        `${process.env.GCNOTIFY_BASE_URL}/v2/notifications/email`,
-        {
-          email_address: emailProps.mpoEmail,
-          template_id: process.env.GCNOTIFY_TEMPLATE_ID,
-          personalisation: {
-            name: emailProps.mpoName,
-            url: emailProps.url,
-            piaTitle: emailProps.piaTitle,
-            piaId: emailProps.piaId,
+      this.httpService
+        .post(
+          `${process.env.GCNOTIFY_BASE_URL}/v2/notifications/email`,
+          {
+            email_address: emailProps.mpoEmail,
+            template_id: process.env.GCNOTIFY_TEMPLATE_ID,
+            personalisation: {
+              name: emailProps.mpoName,
+              url: emailProps.url,
+              piaTitle: emailProps.piaTitle,
+              piaId: emailProps.piaId,
+            },
           },
-        },
-        {
-          headers: {
-            Authorization: `ApiKey-v1 ${process.env.GCNOTIFY_API_KEY}`,
-            'Content-Type': 'application/json',
+          {
+            headers: {
+              Authorization: `ApiKey-v1 ${process.env.GCNOTIFY_API_KEY}`,
+              'Content-Type': 'application/json',
+            },
           },
-        },
-      ),
+        )
+        .pipe(
+          map((response: any) => response.data),
+          catchError((e) => {
+            console.error(e.response.data, e.response.status);
+            throw new HttpException(e.response.data, e.response.status);
+          }),
+        ),
     );
     return data.statusText;
   }
