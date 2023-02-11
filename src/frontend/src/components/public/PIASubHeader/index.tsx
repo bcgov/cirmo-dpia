d7bc4d096e40d2ef926598cf67af595bb0d5756c
bing gao <bing@freshworks.io>
2023-02-10 17:00:11 -0800

    update css for subheader

--- src/frontend/src/components/public/PIASubHeader/index.tsx
+++ src/frontend/src/components/public/PIASubHeader/index.tsx
@@ -4,6 +4,13 @@ import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import { statusList } from '../../../utils/status';
 import { PIASubHeaderProps } from './interfaces';
 import Alert from '../../common/Alert';
+import { useState } from 'react';
+import {
+  FileDownload,
+  FileDownloadTypeEnum,
+} from '../../../utils/file-download.util';
+import { API_ROUTES } from '../../../constant/apiRoutes';
+import Spinner from '../../common/Spinner';
 
 function PIASubHeader({
   pia,
@@ -11,11 +18,50 @@ function PIASubHeader({
   onSaveChangeClick = () => {},
   onSubmitClick = () => {},
 }: PIASubHeaderProps) {
+  //TODO implement tooltip for faEllipsisH icon, so when mouse hover, will display download word
+  // this pr just add download function for faEllipsisH icon
+  const [isDownloading, setIsDownloading] = useState(false);
+  const [downloadError, setDownloadError] = useState('');
+  const handleDownload = async () => {
+    setDownloadError('');
+
+    if (!pia.id) {
+      console.error(
+        'Something went wrong. Result Id not available for download',
+      );
+      setDownloadError('Something went wrong. Please try again.');
+      return;
+    }
+
+    setIsDownloading(true);
+    try {
+      await FileDownload.download(
+        API_ROUTES.PIA_INTAKE_RESULT_DOWNLOAD.replace(':id', `${pia.id}`),
+        FileDownloadTypeEnum.PDF,
+      );
+    } catch (e) {
+      setDownloadError('Something went wrong. Please try again.');
+    } finally {
+      setIsDownloading(false);
+    }
+  };
+  const handleAlertClose = () => {
+    setDownloadError('');
+  };
   return (
     <>
       <div className="subheader-container wrapper">
         <h1>New PIA</h1>
-
+        <div className="col">
+          {downloadError && (
+            <Alert
+              type="danger"
+              message="Something went wrong. Please try again."
+              onClose={handleAlertClose}
+              className="mt-2 col-sm-1"
+            />
+          )}
+        </div>
         <div className=" row ms-auto ">
           <div className="col">
             <div>Status</div>
@@ -35,7 +81,6 @@ function PIASubHeader({
               )}
             </div>
           </div>
-
           {lastSaveAlertInfo.show && (
             <div className="col col-md-4">
               <Alert
@@ -47,8 +92,12 @@ function PIASubHeader({
             </div>
           )}
 
-          <button className="mx-2 bcgovbtn bcgovbtn__secondary">
+          <button
+            onClick={() => handleDownload()}
+            className="mx-2 bcgovbtn bcgovbtn__secondary"
+          >
             <FontAwesomeIcon icon={faEllipsisH} />
+            {isDownloading && <Spinner />}
           </button>
 
           <button
