4f750fd28a0e92e6dc97a47b8f0d27f8b271b470
bing gao <bing@freshworks.io>
2023-02-15 16:10:27 -0800

    add PIB tab

---
+++ src/frontend/src/components/public/PIAPersonalInformationBanks/messages.ts
@@ -0,0 +1,22 @@
+// This object contains all of the text values for the PIA personal informaton banks component
+export default {
+  Headings: {
+    Title: {
+      en: `Personal information banks (PIBs)`,
+    },
+    Description: {
+      en: `A personal information bank (PIB) is a collection of personal information searchable by name or unique identifier.`,
+    },
+  },
+  Section: {
+    Title: {
+      en: `Resulting in a PIB`,
+    },
+    QuestionOne: {
+      en: `Will your initiative result in a personal information bank (PIB)?`,
+    },
+    QuestionOneDescription: {
+      en: `Describe the type of information in the bank`,
+    },
+  },
+};
