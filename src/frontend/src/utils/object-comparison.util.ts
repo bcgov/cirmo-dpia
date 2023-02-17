71f16a3689a9932b6b35594823b8a84755cb2594
Kushal Arora <kushal.arora1@ca.ey.com>
2023-02-17 01:00:19 -0800

    [UTOPIA-806] [Frontend] ensuring autosave works for nested objects - deep equality

--- src/frontend/src/utils/object-comparison.util.ts
+++ src/frontend/src/utils/object-comparison.util.ts
@@ -1,19 +1,35 @@
-export const shallowEqual = (
+export const deepEqual = (
   object1: Record<string, any>,
   object2: Record<string, any>,
   skipKeys: Array<string> = [],
 ) => {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
+
   if (keys1.length !== keys2.length) {
     return false;
   }
+
   for (const key of keys1) {
     if (skipKeys.includes(key)) continue;
 
+    // recursive check for nested values [objects]
+    if (
+      typeof object1[key] === typeof object2[key] &&
+      typeof object1[key] === 'object' &&
+      object1[key] !== null &&
+      object1[key] !== undefined
+    ) {
+      if (deepEqual(object1[key], object2[key])) continue;
+
+      return false;
+    }
+
+    // check for primitive values
     if (object1[key] !== object2[key]) {
       return false;
     }
   }
+
   return true;
 };
