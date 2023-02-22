4280a017e139288cd1da7a23c07cda21321fec37
bing gao <bing@freshworks.io>
2023-02-22 09:08:29 -0800

    update container css width

--- src/frontend/src/components/common/List/index.tsx
+++ src/frontend/src/components/common/List/index.tsx
@@ -33,69 +33,66 @@ const List = ({
   enableRemove = true,
 }: ListProps) => {
   return (
-    <div>
-      <div className="container">
-        <div>
-          <div className="data-table__container ">
-            <table id="tab_logic" className="table data-table">
-              <thead>
-                <tr>
-                  {columnsName.map((column, index) => (
-                    <th key={index}>{column}</th>
-                  ))}
-                  <th />
-                </tr>
-              </thead>
+    <>
+      <div className="data-table__container">
+        <table id="tab_logic" className="table data-table">
+          <thead>
+            <tr>
+              {columnsName.map((column, index) => (
+                <th key={index}>{column}</th>
+              ))}
+              <th />
+            </tr>
+          </thead>
 
-              <tbody>
-                {data.map((items, idx) => (
-                  <tr key={idx}>
-                    {items.map((item, index) => (
-                      <td key={index}>
-                        <div className=" row">
-                          <div className="mt-3 col-sm-2">
-                            {index === 0 ? (
-                              <label className="mt-3 ">
-                                {inputLabel + ' ' + (idx + 1)}
-                              </label>
-                            ) : null}
-                          </div>
-                          <div className=" px-2 col-sm-10">
-                            <InputText
-                              type="text"
-                              value={item.value}
-                              id={item.id}
-                              onChange={(e) => handleOnChange(e, idx, index)}
-                            />
-                          </div>
-                        </div>
-                      </td>
-                    ))}
-
-                    {enableRemove ? (
-                      <td>
-                        <button
-                          className=" btn btn-outline-danger mt-2 "
-                          onClick={() => removeRow(idx)}
-                        >
-                          <FontAwesomeIcon icon={faTrash} />
-                        </button>
-                      </td>
-                    ) : null}
-                  </tr>
+          <tbody>
+            {data.map((items, idx) => (
+              <tr key={idx}>
+                {items.map((item, index) => (
+                  <td key={index}>
+                    <div className="row">
+                      <div className="mt-3 col-sm-2">
+                        {index === 0 ? (
+                          <label className="mt-3 ">
+                            {inputLabel + ' ' + (idx + 1)}
+                          </label>
+                        ) : null}
+                      </div>
+                      <div className="px-2 col-sm-10">
+                        <InputText
+                          type="text"
+                          value={item.value}
+                          id={item.id}
+                          onChange={(e) => handleOnChange(e, idx, index)}
+                        />
+                      </div>
+                    </div>
+                  </td>
                 ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+
+                {enableRemove ? (
+                  <td>
+                    <button
+                      className=" btn btn-outline-danger mt-2 "
+                      onClick={() => removeRow(idx)}
+                    >
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </td>
+                ) : null}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
+
       <div className="pt-4 pb-4 view-pid">
         <button onClick={addRow} className="bcgovbtn bcgovbtn__tertiary  ">
           Add more rows
           <FontAwesomeIcon icon={faPlus} />
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
