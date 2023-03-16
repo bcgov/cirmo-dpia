75ae07673cd03912122d43c8a378772fbf2544a5
bing gao <bing@freshworks.io>
2023-03-16 14:28:53 -0700

    update migration rollback sql order

--- src/backend/src/migrations/1678996557122-update-ministry-name.ts
+++ src/backend/src/migrations/1678996557122-update-ministry-name.ts
@@ -18,16 +18,16 @@ export class UpdateMinistryName1678996557122 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`UPDATE "pia-intake"  SET "ministry" ='ADVANCED_EDUCATION_AND_SKILLS_TRAINING'
-        WHERE ministry='POST_SECONDARY_EDUCATION_AND_FUTURE_SKILLS'`);
-
-    await queryRunner.query(`UPDATE "pia-intake"  SET "ministry" ='JOBS_ECONOMIC_RECOVERY_AND_INNOVATION'
-        WHERE ministry='JOBS_ECONOMIC_DEVELOPMENT_AND_INNOVATION'`);
+    await queryRunner.query(`UPDATE "pia-intake"  SET "ministry" ='PUBLIC_SAFETY_AND_SOLICITOR_GENERAL_AND_EMERGENCY_BC'
+    WHERE ministry='PUBLIC_SAFETY_AND_SOLICITOR_GENERAL'`);
 
     await queryRunner.query(`UPDATE "pia-intake"  SET "ministry" ='LAND_WATER_AND_RESOURCE_STEWARDSHIP'
         WHERE ministry='WATER_LAND_AND_RESOURCE_STEWARDSHIP'`);
 
-    await queryRunner.query(`UPDATE "pia-intake"  SET "ministry" ='PUBLIC_SAFETY_AND_SOLICITOR_GENERAL_AND_EMERGENCY_BC'
-        WHERE ministry='PUBLIC_SAFETY_AND_SOLICITOR_GENERAL'`);
+    await queryRunner.query(`UPDATE "pia-intake"  SET "ministry" ='JOBS_ECONOMIC_RECOVERY_AND_INNOVATION'
+        WHERE ministry='JOBS_ECONOMIC_DEVELOPMENT_AND_INNOVATION'`);
+
+    await queryRunner.query(`UPDATE "pia-intake"  SET "ministry" ='ADVANCED_EDUCATION_AND_SKILLS_TRAINING'
+        WHERE ministry='POST_SECONDARY_EDUCATION_AND_FUTURE_SKILLS'`);
   }
 }
