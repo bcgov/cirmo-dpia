425fbe5f1b7038c12e974dfa071e44723bd3d8fd
bing gao <57013495+binggaofw@users.noreply.github.com>
2023-03-24 08:45:58 -0700

    add footer link (#1008)

--- src/frontend/src/components/common/Footer/Footer.tsx
+++ src/frontend/src/components/common/Footer/Footer.tsx
@@ -5,22 +5,32 @@ function Footer() {
     <footer data-cy="footer" className="footer wrapper">
       <ul>
         <li>
-          <a>Home</a>
+          <a href="https://www2.gov.bc.ca/gov/content/home">Home</a>
         </li>
         <li>
-          <a>About gov.bc.ca</a>
+          <a href="https://www2.gov.bc.ca/gov/content?id=3C4F47288DFB454987435AB5EFEFBB7F">
+            About gov.bc.ca
+          </a>
         </li>
         <li>
-          <a>Disclaimer</a>
+          <a href="https://www2.gov.bc.ca/gov/content?id=79F93E018712422FBC8E674A67A70535">
+            Disclaimer
+          </a>
         </li>
         <li>
-          <a>Accessibility</a>
+          <a href="https://www2.gov.bc.ca/gov/content?id=E08E79740F9C41B9B0C484685CC5E412">
+            Accessibility
+          </a>
         </li>
         <li>
-          <a>Copyright</a>
+          <a href="https://www2.gov.bc.ca/gov/content?id=1AAACC9C65754E4D89A118B875E0FBDA">
+            Copyright
+          </a>
         </li>
         <li>
-          <a>Contact us</a>
+          <a href="https://www2.gov.bc.ca/gov/content?id=6A77C17D0CCB48F897F8598CCC019111">
+            Contact us
+          </a>
         </li>
       </ul>
     </footer>
