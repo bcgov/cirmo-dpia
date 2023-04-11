7a6f0f572e65714760e2a202e9b2eaa4c8517213
Bottle7 <brandonjbouchard@gmail.com>
2023-04-11 10:50:34 -0700

    Collapsible Element
    - Creates & implements a wrapper component that takes in a fontawesome
      icon and an alignment (left or right) and makes it's children
      collapsible

---
+++ src/frontend/src/components/common/Collapsible/interfaces.ts
@@ -0,0 +1,8 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { ReactNode } from 'react';
+
+export default interface CollapsibleProps {
+  icon: IconProp;
+  children: ReactNode;
+  alignment: string;
+}
