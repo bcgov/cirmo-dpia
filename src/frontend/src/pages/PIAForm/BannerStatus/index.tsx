dff113a4a990b19d03621016301c353e8ddffc6f
bing gao <bing@freshworks.io>
2023-04-11 09:41:19 -0700

    update logic and rollback husky hook change

--- src/frontend/src/pages/PIAForm/BannerStatus/index.tsx
+++ src/frontend/src/pages/PIAForm/BannerStatus/index.tsx
@@ -11,16 +11,16 @@ const BannerStatus = ({ pia }: IBannerStatusProps) => {
       setBannerMessage(
         'Warning: Your Ministry Privacy Office (MPO) can’t see or help you with this PIA until you click “Submit”. ',
       );
-    } else if (isMPORole()) {
-      if (pia.status === PiaStatuses.MPO_REVIEW)
+    } else if (pia.status === PiaStatuses.MPO_REVIEW) {
+      if (isMPORole()) {
         setBannerMessage(
           'To submit this PIA to PCT for review, download it as a PDF and send it to PCT via email.',
         );
-    } else {
-      if (pia.status === PiaStatuses.MPO_REVIEW)
+      } else {
         setBannerMessage(
           ' Your PIA is being reviewed by your Ministry Privacy Office (MPO). Your MPO will contact you once it is reviewed.',
         );
+      }
     }
   }, [pia.status]);
 
