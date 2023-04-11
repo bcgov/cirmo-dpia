fe7c60ac826137b0cc5958312fe41a369ec08767
bing gao <bing@freshworks.io>
2023-04-11 16:01:00 -0700

    fix lint error

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'import/named': 'error',
+    'import/named': 'off',
     'jest/no-export': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-child-element-spacing': 'off',
