f6e51a8d84f525a9de629966bd6114235a52848a
akhilfw <akhil@freshworks.io>
2023-04-13 12:53:57 -0700

    UTOPIA-336 update workflow and add login page

--- src/frontend/cypress.config.ts
+++ src/frontend/cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
+    "env": {
+      "PASSWORD": ""
+    }
   },
   video: false,
   defaultCommandTimeout: 5000,
