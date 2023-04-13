4d017f006929fa071afcd4a171d55fcba6ec0522
bing gao <bing@freshworks.io>
2023-04-13 14:36:36 -0700

    [UTOPIA-1021 update callout banner text  for pia

---
+++ src/frontend/src/pages/PIAForm/BannerStatus/messages.ts
@@ -0,0 +1,15 @@
+export default {
+  MPOReviewStatusCalloutText: {
+    Drafter: {
+      en: `Your PIA is being reviewed by your Ministry Privacy Office (MPO). Your MPO will contact you once it is reviewed.`,
+    },
+    MPO: {
+      en: `To submit this PIA to PCT for review, download it as a PDF and send it to PCT via email.`,
+    },
+  },
+  InCompleteStatusCalloutText: {
+    Drafter: {
+      en: `Your PIA is in Incomplete status. Your Ministry Privacy Office (MPO) will only be able to view and help you with this PIA after you click “Submit”`,
+    },
+  },
+};
