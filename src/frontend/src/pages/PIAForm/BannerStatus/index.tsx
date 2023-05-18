c7ad39418d0a0b3c0b6d883d85b7c56a5737c2b2
bing gao <bing@freshworks.io>
2023-05-18 14:34:03 -0700

    [UTOPIA-1002]add banner info for cpo_review status

--- src/frontend/src/pages/PIAForm/BannerStatus/index.tsx
+++ src/frontend/src/pages/PIAForm/BannerStatus/index.tsx
@@ -2,21 +2,28 @@ import { useState, useEffect } from 'react';
 import { IBannerStatusProps } from './interfaces';
 import Callout from '../../../components/common/Callout';
 import { PiaStatuses } from '../../../constant/constant';
-import { isMPORole } from '../../../utils/helper.util';
+import { isMPORole, getGUID } from '../../../utils/helper.util';
 import Messages from './messages';
 const BannerStatus = ({ pia }: IBannerStatusProps) => {
   const [bannerMessage, setBannerMessage] = useState<string>('');
+  const owner = getGUID() === pia.createdByGuid ? true : false;
   useEffect(() => {
     if (pia.status === PiaStatuses.INCOMPLETE) {
       setBannerMessage(Messages.InCompleteStatusCalloutText.Drafter.en);
     } else if (pia.status === PiaStatuses.MPO_REVIEW) {
       if (isMPORole()) {
         setBannerMessage(Messages.MPOReviewStatusCalloutText.MPO.en);
-      } else {
+      } else if (owner) {
         setBannerMessage(Messages.MPOReviewStatusCalloutText.Drafter.en);
       }
+    } else if (pia.status === PiaStatuses.CPO_REVIEW) {
+      if (isMPORole()) {
+        setBannerMessage(Messages.CPOReviewStatusCalloutText.MPO.en);
+      } else if (owner) {
+        setBannerMessage(Messages.CPOReviewStatusCalloutText.Drafter.en);
+      }
     }
-  }, [pia.status]);
+  }, [owner, pia.status]);
 
   return pia.status !== PiaStatuses.EDIT_IN_PROGRESS ? (
     <div className="mb-5">
--- src/frontend/src/pages/PIAForm/BannerStatus/messages.ts
+++ src/frontend/src/pages/PIAForm/BannerStatus/messages.ts
@@ -7,6 +7,14 @@ export default {
       en: `To submit this PIA to the CPO for review, download it as a PDF and send it to the CPO via email.`,
     },
   },
+  CPOReviewStatusCalloutText: {
+    Drafter: {
+      en: `Your PIA is being reviewed by the Corporate Privacy Office (CPO). CPO or your MPO will contact you with any questions.`,
+    },
+    MPO: {
+      en: `This PIA is being reviewed by the Corporate Privacy Office (CPO). CPO will contact you with any questions.`,
+    },
+  },
   InCompleteStatusCalloutText: {
     Drafter: {
       en: `Your PIA is in Incomplete status. Your Ministry Privacy Office (MPO) will only be able to view and help you with this PIA after you click “Submit”.`,
