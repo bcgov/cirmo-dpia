ead8bb5f22455ddc9e75e1c63509dd1593cc5302
Kushal Arora <kushal.arora1@ca.ey.com>
2023-06-12 12:07:01 -0700

    lint fix

--- src/backend/src/migrations/1686005634115-invites-entity.ts
+++ src/backend/src/migrations/1686005634115-invites-entity.ts
@@ -5,7 +5,7 @@ export class InvitesEntity1686005634115 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
-    
+
     await queryRunner.query(
       `CREATE TABLE "invites" ("id" SERIAL NOT NULL, "is_active" boolean NOT NULL DEFAULT true, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "created_by_guid" character varying NOT NULL, "created_by_username" character varying NOT NULL, "updated_by_guid" character varying NOT NULL, "updated_by_username" character varying NOT NULL, "code" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_by_display_name" character varying NOT NULL, "pia_id" integer NOT NULL, CONSTRAINT "REL_e9fbb70df855a8f96b9ab658c3" UNIQUE ("pia_id"), CONSTRAINT "PK_aa52e96b44a714372f4dd31a0af" PRIMARY KEY ("id"))`,
     );
