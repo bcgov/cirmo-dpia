f9f8c8b5cabe0f2d8650e49ce5309fe7cc4e9de6
bing gao <bing@freshworks.io>
2023-06-26 16:23:46 -0700

    [UTOPIA-1152] update mpo review section ui

--- src/frontend/src/components/public/PIAFormTabs/review/interfaces.ts
+++ src/frontend/src/components/public/PIAFormTabs/review/interfaces.ts
@@ -5,8 +5,14 @@ export interface IReview {
   mpo: {
     isAcknowledged: boolean;
     reviewNote: string;
+<<<<<<< HEAD
     reviewedByDisplayName?: string;
     reviewedByGuid?: string;
     reviewedAt?: string;
+=======
+    reviewedByDisplayName: string;
+    reviewedByGUID: string;
+    reviewedAt: string;
+>>>>>>> bed54871 ([UTOPIA-1152] update mpo review section ui)
   };
 }
