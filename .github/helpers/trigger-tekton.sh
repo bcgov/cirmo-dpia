714413e7e2d9b5ab83eb647cc5b97c414b7b19c8
Adam Kroon <adam.kroon@gov.bc.ca>
2023-07-05 14:20:56 -0700

    adding correct syntax for injecting environment variables into curl raw json

--- .github/helpers/trigger-tekton.sh
+++ .github/helpers/trigger-tekton.sh
@@ -3,4 +3,4 @@
 # script used to trigger tekton event listener and supply task with required
 # information required to deploy specific service into a specific environment
 
-curl --location --request POST $templateURL --header 'Content-Type: application/json' --data-raw '{"environment": $ENVIRONMENT, "application": $APPLICATION, "image": $IMAGE, "templateURL": $TEMPLATE_URL}'
+curl --location --request POST "'"$EL_ROUTE"'" --header 'Content-Type: application/json' --data-raw '{"environment": "'"$ENVIRONMENT"'", "application": "'"$APPLICATION"'", "image": "'"$IMAGE"'", "templateURL": "'"$TEMPLATE_URL"'"}'
