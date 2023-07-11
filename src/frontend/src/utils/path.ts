bd84ccf7252d8347c92d94f3e4104b200ef9d696
Shivakumar <Anthony.Shivakumar@gov.bc.ca>
2023-07-11 12:19:45 -0700

    Fix edit link issues when in review tab

--- src/frontend/src/utils/path.ts
+++ src/frontend/src/utils/path.ts
@@ -12,3 +12,29 @@ export const buildDynamicPath = (
     return acc?.replace(`:${key}`, value.toString());
   }, route);
 };
+
+/*
+ * @param path - the path to be converted
+ * @returns the converted path
+ * @example getEditPath('/pia/1/intake/view') => '/pia/1/intake/edit'
+ * @description This function is used to convert the path from view to edit
+ * it splits up the path into an array and searches for the view keyword
+ * and replaces it with edit
+ * If there are tabs that do not have an edit version for example the review tab
+ * it will replace it with intake
+ *
+ */
+export const getEditPath = (path: string): string => {
+  if (!path) return '';
+  const pathArray = path.split('/');
+  pathArray.forEach((element, index) => {
+    if (element === 'view') {
+      pathArray[index] = 'edit';
+    }
+    if (element === 'review') {
+      pathArray[index] = 'intake';
+    }
+  });
+  path = pathArray.join('/');
+  return path;
+};
