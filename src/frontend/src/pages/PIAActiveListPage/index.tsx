1ccc4868de9a00a5e60401a8a400eeec411f96ae
Shivakumar <Anthony.Shivakumar@gov.bc.ca>
2023-07-21 10:21:10 -0700

    change title to active PIA

--- src/frontend/src/pages/PIAActiveListPage/index.tsx
+++ src/frontend/src/pages/PIAActiveListPage/index.tsx
@@ -2,7 +2,7 @@ import ListViewRender from '../../components/public/ListViewRender';
 
 const PIAActiveList = () => {
   return <ListViewRender showCompleted={false}
-  title={`List of PIA's`} />;
+  title={`Active PIA's`} />;
 };
 
 export default PIAActiveList;
