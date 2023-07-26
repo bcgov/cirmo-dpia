718ab410bb99097b3b9bdb0994e2a55a807de11f
Brandon Bouchard <brandonjbouchard@gmail.com>
2023-07-26 09:56:58 -0700

    Change "Before you Begin" list items on PIA Intake page

--- src/frontend/src/components/public/PIAFormTabs/intake/helper/messages.ts
+++ src/frontend/src/components/public/PIAFormTabs/intake/helper/messages.ts
@@ -8,9 +8,6 @@ export default {
     },
     ListText: [
       {
-        en: 'Fill out this form if you work or are a service provider to a ministry in the Government of B.C. and are starting a new initiative or significantly changing an existing initiative',
-      },
-      {
         en: 'Do your best to fill out the fields below, but know that you can return to this section and edit your answers in the future',
       },
       {
@@ -18,6 +15,9 @@ export default {
       },
       {
         en: 'After you submit this form, your MPO will be able to see it and help you with any questions you might have',
+      },
+      {
+        en: "If you are updating an existing PIA or drafting a PIA for legislation or regulations, find the PIA template you need here"
       },
     ],
   },
@@ -41,9 +41,9 @@ export default {
       en: `What is the initiative?`,
     },
     HelperText: {
-      en: `Describe your initiative in enough detail that a reader who knows nothing 
-          about your work will understand the purpose of your initiative and who your 
-          partners and other stakeholders are. Describe what you’re doing, how it works, 
+      en: `Describe your initiative in enough detail that a reader who knows nothing
+          about your work will understand the purpose of your initiative and who your
+          partners and other stakeholders are. Describe what you’re doing, how it works,
           who is involved and when or how long your initiative runs.`,
     },
   },
@@ -52,8 +52,8 @@ export default {
       en: `What is the scope of the PIA?`,
     },
     HelperText: {
-      en: `Your initiative might be part of a larger one or might be rolled out in 
-          phases. What part of the initiative is covered by this PIA? What is out of 
+      en: `Your initiative might be part of a larger one or might be rolled out in
+          phases. What part of the initiative is covered by this PIA? What is out of
           scope of this PIA?`,
     },
   },
@@ -62,9 +62,9 @@ export default {
       en: `What are the data or information elements involved in your initiative?`,
     },
     HelperText: {
-      en: `Please list all the elements of information or data that you might collect, 
-          use, store, disclose, or access as part of your initiative. If your initiative 
-          involves large quantities of information or datasets, you can list categories 
+      en: `Please list all the elements of information or data that you might collect,
+          use, store, disclose, or access as part of your initiative. If your initiative
+          involves large quantities of information or datasets, you can list categories
           or other groupings of personal information in a table below or in an appendix.`,
     },
   },
@@ -79,19 +79,19 @@ export default {
       en: `Personal information`,
     },
     HelperText: {
-      en: `is any recorded information about an identifiable individual, other than 
-          business contact information. Personal information includes information that 
+      en: `is any recorded information about an identifiable individual, other than
+          business contact information. Personal information includes information that
           can be used to identify an individual through association or reference.`,
     },
   },
   InitiativeRiskReductionSection: {
     H2Text: {
-      en: `How will you reduce the risk of unintentionally collecting personal 
+      en: `How will you reduce the risk of unintentionally collecting personal
           information?`,
     },
     HelperText: {
-      en: `Some initiatives that do not require personal information are at risk of 
-          collecting personal information inadvertently, which could result in an information 
+      en: `Some initiatives that do not require personal information are at risk of
+          collecting personal information inadvertently, which could result in an information
           incident or privacy breach. `,
     },
   },
