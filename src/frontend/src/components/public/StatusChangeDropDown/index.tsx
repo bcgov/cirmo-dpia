23bcb009e31a47d533c61c78349ffc74b2fd9e2e
BradyMitch <braden.jr.mitch@gmail.com>
2023-09-25 15:59:20 -0700

    clean up

--- src/frontend/src/components/public/StatusChangeDropDown/index.tsx
+++ src/frontend/src/components/public/StatusChangeDropDown/index.tsx
@@ -41,11 +41,40 @@ function StatusChangeDropDown({
       statuses: Array.from(statusesMap.values()),
     };
   };
-
   const { hasStatusDropdown, statuses } = checkPrivileges();
 
+  // Track selected status when using keyboard naviagation.
   const itemRefs = useRef<HTMLLIElement[] | null[]>([]);
   const [selectedStatusIndex, setSelectedStatusIndex] = useState<number>(0);
+
+  const handleNavigation = (direction: string) => {
+    if (direction === 'down') {
+      // Key pressed down or right.
+      setSelectedStatusIndex((prev) => {
+        if (prev === statuses.length - 1) {
+          return 0; // Wrap around to the start
+        }
+        return prev + 1; // Move to the next status
+      });
+    } else if (direction == 'up') {
+      // Key pressed up or left.
+      setSelectedStatusIndex((prev) => {
+        if (prev - 1 < 0) {
+          return statuses.length - 1; // Wrap around to the end
+        }
+        return prev - 1; // Move to the prev status
+      });
+    }
+  };
+
+  const handleKeyUp = (event: React.KeyboardEvent) => {
+    event.preventDefault();
+    if (['ArrowDown', 'ArrowRight'].includes(event.key)) {
+      handleNavigation('down');
+    } else if (['ArrowUp', 'ArrowLeft'].includes(event.key)) {
+      handleNavigation('up');
+    }
+  };
 
   const handleKeyDown = (
     event: React.KeyboardEvent,
@@ -58,29 +87,36 @@ function StatusChangeDropDown({
     }
   };
 
-  const handleKeyUp = (event: React.KeyboardEvent) => {
-    event.preventDefault();
-    // Check if "Down" key is pressed
-    if (event.key === 'ArrowDown') {
-      setSelectedStatusIndex((prev) => {
-        if (prev === statuses.length - 1) {
-          return 0; // Wrap around to the start
-        }
-        return prev + 1; // Move to the next status
-      });
-    } else if (event.key === 'ArrowUp') {
-      setSelectedStatusIndex((prev) => {
-        if (prev - 1 < 0) {
-          return statuses.length - 1; // Wrap around to the end
-        }
-        return prev - 1; // Move to the prev status
-      });
-    }
-  };
+  // Focus on the first item in the dropdown list.
+  const setInitialFocus = () => itemRefs.current[0]?.focus();
 
   useEffect(() => {
     itemRefs.current[selectedStatusIndex]?.focus();
   }, [selectedStatusIndex]);
+
+  const DropdownStatusItems = () => {
+    return statuses.map((statuskey, index) => (
+      <li
+        key={index}
+        ref={(element) => (itemRefs.current[index] = element)}
+        tabIndex={index === selectedStatusIndex ? 0 : -1}
+        onKeyDown={(event) => handleKeyDown(event, statuskey)}
+        onKeyUp={(event) => handleKeyUp(event)}
+        onClick={() => {
+          populateModal(pia, statuskey.status, changeStatusFn);
+        }}
+        className="dropdown-item-container"
+      >
+        <div
+          className={`dropdown-item statusBlock ${
+            statusList(null)[statuskey.status].class
+          }`}
+        >
+          {statusList(null)[statuskey.status].title}
+        </div>
+      </li>
+    ));
+  };
 
   return (
     <>
@@ -94,66 +130,37 @@ function StatusChangeDropDown({
               id="dropdownMenuButton1"
               data-bs-toggle="dropdown"
               aria-expanded="false"
-              onKeyUp={() => itemRefs.current[0]?.focus()}
+              onKeyUp={setInitialFocus}
             >
               <div
                 className={`statusBlock statusBlock--active ${
                   pia.status && statusList(null)[pia.status].class
                 }`}
               >
-                {pia.status
-                  ? statusList(null)[pia.status].title
-                  : statusList(null)[PiaStatuses.INCOMPLETE].title}
+                {statusList(null)[pia?.status ?? PiaStatuses.INCOMPLETE].title}
               </div>
               <FontAwesomeIcon className="dropdown-icon" icon={faChevronDown} />
             </button>
-            {pia.status ? (
-              pia.status in statusList(null) ? (
-                <ul
-                  className="dropdown-menu"
-                  aria-labelledby="dropdownMenuButton1"
-                >
-                  {statuses.map((statuskey, index) => (
-                    <li
-                      key={index}
-                      ref={(element) => (itemRefs.current[index] = element)}
-                      tabIndex={index === selectedStatusIndex ? 0 : -1}
-                      onKeyDown={(event) => handleKeyDown(event, statuskey)}
-                      onKeyUp={(event) => handleKeyUp(event)}
-                      onClick={() => {
-                        populateModal(pia, statuskey.status, changeStatusFn);
-                      }}
-                      className="dropdown-item-container"
-                    >
-                      <div
-                        className={`dropdown-item statusBlock ${
-                          statusList(null)[statuskey.status].class
-                        }`}
-                      >
-                        {statusList(null)[statuskey.status].title}
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                ''
-              )
+            {pia.status && pia.status in statusList(null) ? (
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <DropdownStatusItems />
+              </ul>
             ) : (
               ''
             )}
           </div>
-        ) : pia.status ? (
-          pia.status in statusList(null) ? (
+        ) : (
+          pia.status &&
+          pia.status in statusList(null) && (
             <div
               className={`statusBlock ${statusList(null)[pia.status].class}`}
             >
               {pia.status ? statusList(null)[pia.status].title : 'Completed'}
             </div>
-          ) : (
-            ''
           )
-        ) : (
-          ''
         )}
       </div>
     </>
