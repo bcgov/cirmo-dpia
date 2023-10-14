e58cae8df5d5084dd60412c9a36ab6256826bac8
Noor Chasib <noorchasib@gmail.com>
2023-10-14 14:41:24 -0700

    refactor and add comments to IntakeInitiativeScope

--- src/frontend/src/components/public/PIAFormTabs/intake/components/IntakeInitiativeScope.tsx
+++ src/frontend/src/components/public/PIAFormTabs/intake/components/IntakeInitiativeScope.tsx
@@ -12,50 +12,63 @@ const IntakeInitiativeScope: React.FC<IntakeInitiativeScopeProps> = ({
   stateChangeHandler,
   commentCount,
 }) => {
+  // Determine section focus based on selectedSection
+  const sectionFocus =
+    selectedSection === PiaSections.INTAKE_INITIATIVE_DETAILS_SCOPE
+      ? 'section-focus'
+      : '';
+
+  // Render the appropriate initiative scope content based on `isReadOnly` and `intakeForm`
+  const renderInitiativeScope = () => {
+    if (isReadOnly) {
+      return intakeForm.initiativeScope ? (
+        <MDEditor.Markdown
+          source={intakeForm.initiativeScope}
+          aria-label="Initiative Scope Textarea Input Preview"
+        />
+      ) : (
+        <p>
+          <i>Not answered</i>
+        </p>
+      );
+    }
+    return (
+      <MDEditor
+        preview="edit"
+        value={intakeForm?.initiativeScope}
+        defaultTabEnable={true}
+        onChange={(value) => stateChangeHandler(value, 'initiativeScope')}
+        aria-label="Initiative Scope Textarea Input"
+      />
+    );
+  };
+
   return (
     <section className="section__padding-block">
-      <div
-        className={`drop-shadow card p-4 p-md-5  ${
-          selectedSection &&
-          selectedSection === PiaSections.INTAKE_INITIATIVE_DETAILS_SCOPE
-            ? 'section-focus'
-            : ''
-        }`}
-      >
-        {!isReadOnly ? (
+      {/* Main card layout */}
+      <div className={`drop-shadow card p-4 p-md-5 ${sectionFocus}`}>
+        {/* Render header */}
+        {isReadOnly ? (
+          <h4>{Messages.InitiativeScopeSection.H2Text.en} </h4>
+        ) : (
           <p className="form__h2">
             <strong>{Messages.InitiativeScopeSection.H2Text.en}</strong>
           </p>
-        ) : (
-          <h4>{Messages.InitiativeScopeSection.H2Text.en} </h4>
         )}
+
+        {/* Render helper text */}
         {!isReadOnly && (
           <p className="form__helper-text">
             {Messages.InitiativeScopeSection.HelperText.en}
           </p>
         )}
+
+        {/* Render initiative scope */}
         <div className="richText" id="initiativeScope">
-          {(isReadOnly && !intakeForm.initiativeScope) ||
-          (isReadOnly && intakeForm.initiativeScope === '') ? (
-            <p>
-              <i>Not answered</i>
-            </p>
-          ) : isReadOnly ? (
-            <MDEditor.Markdown
-              source={intakeForm.initiativeScope}
-              aria-label="Initiative Scope Textarea Input Preview"
-            />
-          ) : (
-            <MDEditor
-              preview="edit"
-              value={intakeForm?.initiativeScope}
-              defaultTabEnable={true}
-              onChange={(value) => stateChangeHandler(value, 'initiativeScope')}
-              aria-label="Initiative Scope Textarea Input"
-            />
-          )}
+          {renderInitiativeScope()}
         </div>
 
+        {/* Component to display comments */}
         <ViewComments
           count={commentCount?.[PiaSections.INTAKE_INITIATIVE_DETAILS_SCOPE]}
           path={PiaSections.INTAKE_INITIATIVE_DETAILS_SCOPE}
