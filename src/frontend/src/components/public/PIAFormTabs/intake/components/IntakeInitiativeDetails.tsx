ed2e9d5ee78f1e57301342e8e34e6e3aaf032004
Noor Chasib <noorchasib@gmail.com>
2023-10-14 14:37:11 -0700

    refactor and add comments to IntakeInitiativeDetails

--- src/frontend/src/components/public/PIAFormTabs/intake/components/IntakeInitiativeDetails.tsx
+++ src/frontend/src/components/public/PIAFormTabs/intake/components/IntakeInitiativeDetails.tsx
@@ -5,6 +5,10 @@ import Messages from '../helper/messages';
 import { IntakeInitiativeDetailsProps } from '../helper/pia-form-intake.interface';
 import { PiaSections } from '../../../../../types/enums/pia-sections.enum';
 
+// Destructure the messages object for better readability
+const { H2Text, HelperText } = Messages.InitiativeDataElementsSection;
+
+// Define the functional component with its expected props
 const IntakeInitiativeDetails: React.FC<IntakeInitiativeDetailsProps> = ({
   isReadOnly,
   selectedSection,
@@ -12,53 +16,59 @@ const IntakeInitiativeDetails: React.FC<IntakeInitiativeDetailsProps> = ({
   stateChangeHandler,
   commentCount,
 }) => {
+  // Calculate if the current section is focused
+  const isSectionFocused =
+    selectedSection ===
+    PiaSections.INTAKE_INITIATIVE_DETAILS_DATA_ELEMENTS_INVOLVED;
+
+  // Generate content based on whether it's read-only mode or not
+  const content =
+    (isReadOnly && !intakeForm.dataElementsInvolved) ||
+    (isReadOnly && intakeForm.dataElementsInvolved === '') ? (
+      <p>
+        <i>Not answered</i>
+      </p>
+    ) : isReadOnly ? (
+      <MDEditor.Markdown
+        source={intakeForm.dataElementsInvolved}
+        aria-label="Data Elements Involved Textarea Input Preview"
+      />
+    ) : (
+      <MDEditor
+        preview="edit"
+        value={intakeForm?.dataElementsInvolved}
+        defaultTabEnable={true}
+        onChange={(value) => stateChangeHandler(value, 'dataElementsInvolved')}
+        aria-label="Data Elements Involved Textarea Input"
+      />
+    );
+
+  // Main render function for the component
   return (
     <section className="section__padding-block">
       <div
-        className={`drop-shadow card p-4 p-md-5  ${
-          selectedSection &&
-          selectedSection ===
-            PiaSections.INTAKE_INITIATIVE_DETAILS_DATA_ELEMENTS_INVOLVED
-            ? 'section-focus'
-            : ''
+        className={`drop-shadow card p-4 p-md-5 ${
+          isSectionFocused ? 'section-focus' : ''
         }`}
       >
-        {!isReadOnly ? (
-          <p className="form__h2">
-            <strong>{Messages.InitiativeDataElementsSection.H2Text.en}</strong>
-          </p>
+        {/* Render the section title */}
+        {isReadOnly ? (
+          <h4>{H2Text.en}</h4>
         ) : (
-          <h4>{Messages.InitiativeDataElementsSection.H2Text.en} </h4>
-        )}
-        {!isReadOnly && (
-          <p className="form__helper-text">
-            {Messages.InitiativeDataElementsSection.HelperText.en}
+          <p className="form__h2">
+            <strong>{H2Text.en}</strong>
           </p>
         )}
+
+        {/* Render the helper text */}
+        {!isReadOnly && <p className="form__helper-text">{HelperText.en}</p>}
+
+        {/* Render the content */}
         <div className="richText" id="dataElementsInvolved">
-          {(isReadOnly && !intakeForm.dataElementsInvolved) ||
-          (isReadOnly && intakeForm.dataElementsInvolved === '') ? (
-            <p>
-              <i>Not answered</i>
-            </p>
-          ) : isReadOnly ? (
-            <MDEditor.Markdown
-              source={intakeForm.dataElementsInvolved}
-              aria-label="Data Elements Involved Textarea Input Preview"
-            />
-          ) : (
-            <MDEditor
-              preview="edit"
-              value={intakeForm?.dataElementsInvolved}
-              defaultTabEnable={true}
-              onChange={(value) =>
-                stateChangeHandler(value, 'dataElementsInvolved')
-              }
-              aria-label="Data Elements Involved Textarea Input"
-            />
-          )}
+          {content}
         </div>
 
+        {/* Render comments */}
         <ViewComments
           count={
             commentCount?.[
