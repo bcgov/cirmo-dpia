d4580afd4fa1f31ecf3b6c5d4f55afa4756449c5
Noor Chasib <noorchasib@gmail.com>
2023-10-16 16:03:09 -0700

    refactor and add comments to PersonalInformationSection

--- src/frontend/src/components/public/PIAFormTabs/accuracyRetention/components/PersonalInformationSection.tsx
+++ src/frontend/src/components/public/PIAFormTabs/accuracyRetention/components/PersonalInformationSection.tsx
@@ -20,199 +20,201 @@ export const PersonalInformationSection: React.FC<
   stateChangeHandler,
   willProvideInformation,
   haveApprovedInfoSchedule,
-}) => (
-  <section className="section__padding-block">
-    <h3 className="form__h2">
-      {Messages.FormElements.PersonalInformationDecision.H2Text.en}
-    </h3>
-    <div
-      className={`drop-shadow card p-4 p-md-5  ${
-        selectedSection &&
-        selectedSection ===
-          PiaSections.ACCURACY_CORRECTION_AND_RETENTION_RETENTION
-          ? 'section-focus'
-          : ''
-      }`}
-    >
-      {!isReadOnly ? (
-        <p>
-          <strong>
-            {
-              Messages.FormElements.PersonalInformationDecision.usePIForDecision
-                .Question.en
-            }
-          </strong>
-        </p>
-      ) : (
-        <h4>
-          {
-            Messages.FormElements.PersonalInformationDecision.usePIForDecision
-              .Question.en
-          }
-        </h4>
-      )}
-      {!isReadOnly ? (
-        willProvideInformation.map((radio, index) => (
-          <Radio key={index} {...radio} />
-        ))
-      ) : (
-        <p>
-          {accuracyCorrectionAndRetentionForm.retention?.usePIForDecision?.charAt(
-            0,
-          )}
-          {accuracyCorrectionAndRetentionForm.retention?.usePIForDecision
-            ?.slice(1)
-            .toLowerCase()}
-        </p>
-      )}
-      {accuracyCorrectionAndRetentionForm?.retention?.usePIForDecision ===
-        YesNoInput.YES ||
-      !accuracyCorrectionAndRetentionForm?.retention?.usePIForDecision ? (
-        <div className="section__padding-block">
-          {!isReadOnly ? (
-            <>
-              <p>
-                <strong>
-                  {
-                    Messages.FormElements.PersonalInformationDecision
-                      .haveApprovedInfoSchedule.Question.en
-                  }
-                </strong>
-              </p>
-              <p>
-                <a
-                  href={
-                    Messages.FormElements.PersonalInformationDecision
-                      .haveApprovedInfoSchedule.FOIPPALink
-                  }
-                  rel="noreferrer external"
-                  target="_blank"
-                >
-                  {
-                    Messages.FormElements.PersonalInformationDecision
-                      .haveApprovedInfoSchedule.FOIPPAText.en
-                  }
-                  <FontAwesomeIcon
-                    className="helper-text__link-icon"
-                    icon={faUpRightFromSquare}
-                  />
-                </a>
-                {
-                  Messages.FormElements.PersonalInformationDecision
-                    .haveApprovedInfoSchedule.HelperTextPartOne.en
-                }
-                <a
-                  href={
-                    Messages.FormElements.PersonalInformationDecision
-                      .haveApprovedInfoSchedule.IMALink
-                  }
-                  rel="noreferrer external"
-                  target="_blank"
-                >
-                  {
-                    Messages.FormElements.PersonalInformationDecision
-                      .haveApprovedInfoSchedule.IMAText.en
-                  }
-                  <FontAwesomeIcon
-                    className="helper-text__link-icon"
-                    icon={faUpRightFromSquare}
-                  />
-                </a>
+}) => {
+  // Destructure messages from the Messages object
+  const { PersonalInformationDecision } = Messages.FormElements;
 
-                {
-                  Messages.FormElements.PersonalInformationDecision
-                    .haveApprovedInfoSchedule.HelperTextPartTwo.en
-                }
-              </p>
-            </>
-          ) : (
-            <h4>
-              {
-                Messages.FormElements.PersonalInformationDecision
-                  .haveApprovedInfoSchedule.Question.en
-              }
-            </h4>
-          )}
-          {!isReadOnly ? (
-            haveApprovedInfoSchedule.map((radio, index) => (
-              <Radio key={index} {...radio} />
-            ))
-          ) : (
-            <p>
-              {accuracyCorrectionAndRetentionForm.retention.haveApprovedInfoSchedule?.charAt(
-                0,
-              )}
-              {accuracyCorrectionAndRetentionForm.retention.haveApprovedInfoSchedule
-                ?.slice(1)
-                .toLowerCase()}
-            </p>
-          )}
-        </div>
-      ) : null}
-      {accuracyCorrectionAndRetentionForm.retention.usePIForDecision ===
-        YesNoInput.YES ||
-      !accuracyCorrectionAndRetentionForm.retention.usePIForDecision ? (
-        accuracyCorrectionAndRetentionForm.retention
-          .haveApprovedInfoSchedule === YesNoInput.NO ||
-        !accuracyCorrectionAndRetentionForm?.retention
-          ?.haveApprovedInfoSchedule ? (
+  // Define the class name for the section
+  const sectionClassName = `drop-shadow card p-4 p-md-5 ${
+    selectedSection === PiaSections.ACCURACY_CORRECTION_AND_RETENTION_RETENTION
+      ? 'section-focus'
+      : ''
+  }`;
+
+  return (
+    <section className="section__padding-block">
+      <h3 className="form__h2">{PersonalInformationDecision.H2Text.en}</h3>
+      <div className={sectionClassName}>
+        {/* Render the question and helper text */}
+        {!isReadOnly ? (
+          <p>
+            <strong>
+              {PersonalInformationDecision.usePIForDecision.Question.en}
+            </strong>
+          </p>
+        ) : (
+          <h4>{PersonalInformationDecision.usePIForDecision.Question.en}</h4>
+        )}
+        {/* Render the radio buttons for willProvideInformation */}
+        {!isReadOnly &&
+          willProvideInformation.map((radio, index) => (
+            <Radio key={index} {...radio} />
+          ))}
+        {/* Render the answer for willProvideInformation */}
+        {isReadOnly && (
+          <p>
+            {accuracyCorrectionAndRetentionForm.retention?.usePIForDecision?.charAt(
+              0,
+            )}
+            {accuracyCorrectionAndRetentionForm.retention?.usePIForDecision
+              ?.slice(1)
+              .toLowerCase()}
+          </p>
+        )}
+        {/* Render the section for haveApprovedInfoSchedule */}
+        {(accuracyCorrectionAndRetentionForm?.retention?.usePIForDecision ===
+          YesNoInput.YES ||
+          !accuracyCorrectionAndRetentionForm?.retention?.usePIForDecision) && (
           <div className="section__padding-block">
             {!isReadOnly ? (
-              <p>
-                <strong>
+              <>
+                <p>
+                  <strong>
+                    {
+                      PersonalInformationDecision.haveApprovedInfoSchedule
+                        .Question.en
+                    }
+                  </strong>
+                </p>
+                <p>
+                  <a
+                    href={
+                      PersonalInformationDecision.haveApprovedInfoSchedule
+                        .FOIPPALink
+                    }
+                    rel="noreferrer external"
+                    target="_blank"
+                  >
+                    {
+                      PersonalInformationDecision.haveApprovedInfoSchedule
+                        .FOIPPAText.en
+                    }
+                    <FontAwesomeIcon
+                      className="helper-text__link-icon"
+                      icon={faUpRightFromSquare}
+                    />
+                  </a>
                   {
-                    Messages.FormElements.PersonalInformationDecision
-                      .describeRetention.Question.en
+                    PersonalInformationDecision.haveApprovedInfoSchedule
+                      .HelperTextPartOne.en
                   }
-                </strong>
-              </p>
+                  <a
+                    href={
+                      PersonalInformationDecision.haveApprovedInfoSchedule
+                        .IMALink
+                    }
+                    rel="noreferrer external"
+                    target="_blank"
+                  >
+                    {
+                      PersonalInformationDecision.haveApprovedInfoSchedule
+                        .IMAText.en
+                    }
+                    <FontAwesomeIcon
+                      className="helper-text__link-icon"
+                      icon={faUpRightFromSquare}
+                    />
+                  </a>
+                  {
+                    PersonalInformationDecision.haveApprovedInfoSchedule
+                      .HelperTextPartTwo.en
+                  }
+                </p>
+              </>
             ) : (
               <h4>
                 {
-                  Messages.FormElements.PersonalInformationDecision
-                    .describeRetention.Question.en
+                  PersonalInformationDecision.haveApprovedInfoSchedule.Question
+                    .en
                 }
               </h4>
             )}
-            {!isReadOnly ? (
-              <MDEditor
-                preview="edit"
-                defaultTabEnable={true}
-                value={
-                  accuracyCorrectionAndRetentionForm?.retention
-                    ?.describeRetention || undefined
-                }
-                onChange={(value) =>
-                  stateChangeHandler(value, 'retention.describeRetention')
-                }
-                aria-label="Describe Retention Textarea Input"
-              />
-            ) : accuracyCorrectionAndRetentionForm.retention
-                .describeRetention ? (
-              <MDEditor.Markdown
-                source={
-                  accuracyCorrectionAndRetentionForm.retention.describeRetention
-                }
-                aria-label="Describe Retention Textarea Input Preview"
-              />
-            ) : (
+            {/* Render the radio buttons for haveApprovedInfoSchedule */}
+            {!isReadOnly &&
+              haveApprovedInfoSchedule.map((radio, index) => (
+                <Radio key={index} {...radio} />
+              ))}
+            {/* Render the answer for haveApprovedInfoSchedule */}
+            {isReadOnly && (
               <p>
-                <i>Not answered</i>
+                {accuracyCorrectionAndRetentionForm.retention.haveApprovedInfoSchedule?.charAt(
+                  0,
+                )}
+                {accuracyCorrectionAndRetentionForm.retention.haveApprovedInfoSchedule
+                  ?.slice(1)
+                  .toLowerCase()}
               </p>
             )}
+            {/* Render the section for describeRetention */}
+            {(accuracyCorrectionAndRetentionForm.retention.usePIForDecision ===
+              YesNoInput.YES ||
+              !accuracyCorrectionAndRetentionForm.retention.usePIForDecision) &&
+              (accuracyCorrectionAndRetentionForm.retention
+                .haveApprovedInfoSchedule === YesNoInput.NO ||
+                !accuracyCorrectionAndRetentionForm?.retention
+                  ?.haveApprovedInfoSchedule) && (
+                <div className="section__padding-block">
+                  {!isReadOnly ? (
+                    <p>
+                      <strong>
+                        {
+                          PersonalInformationDecision.describeRetention.Question
+                            .en
+                        }
+                      </strong>
+                    </p>
+                  ) : (
+                    <h4>
+                      {
+                        PersonalInformationDecision.describeRetention.Question
+                          .en
+                      }
+                    </h4>
+                  )}
+                  {/* Render the MDEditor for describeRetention */}
+                  {!isReadOnly ? (
+                    <MDEditor
+                      preview="edit"
+                      defaultTabEnable={true}
+                      value={
+                        accuracyCorrectionAndRetentionForm?.retention
+                          ?.describeRetention || undefined
+                      }
+                      onChange={(value) =>
+                        stateChangeHandler(value, 'retention.describeRetention')
+                      }
+                      aria-label="Describe Retention Textarea Input"
+                    />
+                  ) : accuracyCorrectionAndRetentionForm.retention
+                      .describeRetention ? (
+                    <MDEditor.Markdown
+                      source={
+                        accuracyCorrectionAndRetentionForm.retention
+                          .describeRetention
+                      }
+                      aria-label="Describe Retention Textarea Input Preview"
+                    />
+                  ) : (
+                    <p>
+                      <i>Not answered</i>
+                    </p>
+                  )}
+                </div>
+              )}
           </div>
-        ) : null
-      ) : null}
-      {showComments && (
-        <ViewComments
-          count={
-            commentCount?.[
-              PiaSections.ACCURACY_CORRECTION_AND_RETENTION_RETENTION
-            ]
-          }
-          path={PiaSections.ACCURACY_CORRECTION_AND_RETENTION_RETENTION}
-        />
-      )}
-    </div>
-  </section>
-);
+        )}
+        {/* Render the comments section */}
+        {showComments && (
+          <ViewComments
+            count={
+              commentCount?.[
+                PiaSections.ACCURACY_CORRECTION_AND_RETENTION_RETENTION
+              ]
+            }
+            path={PiaSections.ACCURACY_CORRECTION_AND_RETENTION_RETENTION}
+          />
+        )}
+      </div>
+    </section>
+  );
+};
