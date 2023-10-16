262315be643fd78fea737c7f4f49a324a0ef05a2
Noor Chasib <noorchasib@gmail.com>
2023-10-16 15:53:32 -0700

    Update AccuracySection.tsx

--- src/frontend/src/components/public/PIAFormTabs/accuracyRetention/components/AccuracySection.tsx
+++ src/frontend/src/components/public/PIAFormTabs/accuracyRetention/components/AccuracySection.tsx
@@ -14,73 +14,80 @@ export const AccuracySection: React.FC<AccuracySectionProps> = ({
   isReadOnly,
   selectedSection,
   stateChangeHandler,
-}) => (
-  <section className="section__padding-block ">
-    <h3 className="form__h2">{Messages.FormElements.Accuracy.H2Text.en}</h3>
-    <div
-      className={`drop-shadow card p-4 p-md-5 ${
-        selectedSection &&
-        selectedSection ===
-          PiaSections.ACCURACY_CORRECTION_AND_RETENTION_ACCURACY
-          ? 'section-focus'
-          : ''
-      }`}
-    >
-      {!isReadOnly ? (
-        <>
-          <p>
-            <strong>{Messages.FormElements.Accuracy.Question.en}</strong>
-          </p>
-          <p>
-            <a
-              href={Messages.FormElements.Accuracy.HelperText.LinkHref}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {Messages.FormElements.Accuracy.HelperText.LinkText.en}
-              <FontAwesomeIcon icon={faUpRightFromSquare} />
-            </a>
-            {Messages.FormElements.Accuracy.HelperText.en}
-          </p>
-        </>
-      ) : (
-        <h4>{Messages.FormElements.Accuracy.Question.en}</h4>
-      )}
-      <div>
+}) => {
+  // Destructure messages from the Messages object
+  const { Accuracy } = Messages.FormElements;
+
+  // Define the class name for the section
+  const sectionClassName = `drop-shadow card p-4 p-md-5 ${
+    selectedSection === PiaSections.ACCURACY_CORRECTION_AND_RETENTION_ACCURACY
+      ? 'section-focus'
+      : ''
+  }`;
+
+  return (
+    <section className="section__padding-block ">
+      <h3 className="form__h2">{Accuracy.H2Text.en}</h3>
+      <div className={sectionClassName}>
+        {/* Render the question and helper text */}
         {!isReadOnly ? (
-          <MDEditor
-            preview="edit"
-            defaultTabEnable={true}
-            value={
-              accuracyCorrectionAndRetentionForm?.accuracy?.description ||
-              undefined
-            }
-            onChange={(value) =>
-              stateChangeHandler(value, 'accuracy.description')
+          <>
+            <p>
+              <strong>{Accuracy.Question.en}</strong>
+            </p>
+            <p>
+              <a
+                href={Accuracy.HelperText.LinkHref}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {Accuracy.HelperText.LinkText.en}
+                <FontAwesomeIcon icon={faUpRightFromSquare} />
+              </a>
+              {Accuracy.HelperText.en}
+            </p>
+          </>
+        ) : (
+          <h4>{Accuracy.Question.en}</h4>
+        )}
+
+        {/* Render the accuracy description */}
+        <div>
+          {!isReadOnly ? (
+            <MDEditor
+              preview="edit"
+              defaultTabEnable={true}
+              value={accuracyCorrectionAndRetentionForm?.accuracy?.description}
+              onChange={(value) =>
+                stateChangeHandler(value, 'accuracy.description')
+              }
+              aria-label="Accuracy Description Textarea Input"
+            />
+          ) : accuracyCorrectionAndRetentionForm.accuracy?.description ? (
+            <MDEditor.Markdown
+              source={accuracyCorrectionAndRetentionForm.accuracy?.description}
+              aria-label="Accuracy Description Textarea Input Preview"
+            />
+          ) : (
+            <p>
+              <i>Not answered</i>
+            </p>
+          )}
+        </div>
+
+        {/* Render the comments section */}
+        {showComments && (
+          <ViewComments
+            count={
+              commentCount?.[
+                PiaSections.ACCURACY_CORRECTION_AND_RETENTION_ACCURACY
+              ]
             }
             aria-label="Accuracy Description Textarea Input"
+            path={PiaSections.ACCURACY_CORRECTION_AND_RETENTION_ACCURACY}
           />
-        ) : accuracyCorrectionAndRetentionForm.accuracy?.description ? (
-          <MDEditor.Markdown
-            source={accuracyCorrectionAndRetentionForm.accuracy?.description}
-            aria-label="Accuracy Description Textarea Input Preview"
-          />
-        ) : (
-          <p>
-            <i>Not answered</i>
-          </p>
         )}
       </div>
-      {showComments && (
-        <ViewComments
-          count={
-            commentCount?.[
-              PiaSections.ACCURACY_CORRECTION_AND_RETENTION_ACCURACY
-            ]
-          }
-          path={PiaSections.ACCURACY_CORRECTION_AND_RETENTION_ACCURACY}
-        />
-      )}
-    </div>
-  </section>
-);
+    </section>
+  );
+};
