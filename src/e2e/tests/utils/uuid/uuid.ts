fe153d01445a45db4ac3e243626d0cdb0eb391db
Noor Chasib <noorchasib@gmail.com>
2023-11-14 14:31:27 -0800

    10 characters instead of 8 for uuid

--- src/e2e/tests/utils/uuid/uuid.ts
+++ src/e2e/tests/utils/uuid/uuid.ts
@@ -2,7 +2,7 @@ const generateUUID = (): string => {
   const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 10; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
