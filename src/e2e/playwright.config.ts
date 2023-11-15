65b74cbb05c44135c8d59dec08af570c8c63014d
BradyMitch <braden.jr.mitch@gmail.com>
2023-11-15 14:02:00 -0800

    Increased timeout

--- src/e2e/playwright.config.ts
+++ src/e2e/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -27,6 +27,14 @@ export default defineConfig({
     trace: 'on-first-retry',
     ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
+  },
+
+  /* Timeout for test functions */
+  timeout: 60 * 1000,
+
+  /* Timeout for expect functions */
+  expect: {
+    timeout: 30 * 1000,
   },
 
   /* Configure projects for major browsers */
