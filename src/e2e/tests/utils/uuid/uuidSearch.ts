446bf3572cb39bb572ad1ded200487b397886fba
Noor Chasib <noorchasib@gmail.com>
2023-11-14 16:06:14 -0800

    add comments to uuidsearch

--- src/e2e/tests/utils/uuid/uuidSearch.ts
+++ src/e2e/tests/utils/uuid/uuidSearch.ts
@@ -1,21 +1,29 @@
 import { Page, expect } from '@playwright/test';
 
+// Function to search for a PIA using a unique identifier (UUID)
 export async function searchUUID(page: Page, uuid: string) {
+  // Navigate to the PIA list page
   await page.goto('/pia/list');
   await expect(page).toHaveURL('/pia/list');
 
+  // Click on the search input field
   await page.getByPlaceholder('Search by title or drafter').click();
+
+  // Fill the search input field with the unique identifier
   await page
     .getByPlaceholder('Search by title or drafter')
     .fill(`TEST_${uuid}`);
+
+  // Click the search submit button
   await page.getByLabel('Search submit button').click();
 
+  // Click on the cell corresponding to the searched PIA
   await page.getByRole('cell', { name: `TEST_${uuid}` }).click();
 
-  //check for and expect to have the right url
+  // Check and expect the URL to be on the PIA intake view page
   await expect(page).toHaveURL(/\/intake\/view$/);
 
-  //check for and expect title to have the right name
+  // Check and expect the heading (title) to match the unique identifier
   await expect(page.getByRole('heading', { name: `TEST_${uuid}` })).toHaveText(
     `TEST_${uuid}`,
   );
