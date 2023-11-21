74b8c0702fbecaf183e2fd935ce97058b0492be2
Noor Chasib <noorchasib@gmail.com>
2023-11-20 16:52:57 -0800

    add ability to open and close modal

--- src/frontend/src/components/common/ImageMagnifier/index.tsx
+++ src/frontend/src/components/common/ImageMagnifier/index.tsx
@@ -1,12 +1,48 @@
+import React, { useState } from 'react';
 import { ImageMagnifierProps } from './interfaces';
 
 const ImageMagnifier = ({ src, alt }: ImageMagnifierProps) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [zoomLevel, setZoomLevel] = useState(1);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  const handleScroll = (e: React.WheelEvent<HTMLImageElement>) => {
+    const zoomAdjustment = e.deltaY * -0.01;
+    setZoomLevel((prevZoom) =>
+      Math.min(Math.max(prevZoom + zoomAdjustment, 1), 3),
+    );
+  };
+
   return (
-    <img
-      className="rounded mx-auto d-block infographic-diagram"
-      src={src}
-      alt={alt}
-    />
+    <>
+      <img
+        className="rounded mx-auto d-block infographic-diagram"
+        src={src}
+        alt={alt}
+        onClick={openModal}
+      />
+      {modalOpen && (
+        <div className="nextSteps-modal-overlay" onClick={closeModal}>
+          <div
+            className="nextSteps-modal-content"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button className="nextSteps-close-button" onClick={closeModal}>
+              X
+            </button>
+            <img
+              className="nextSteps-zoomable-image"
+              src={src}
+              alt={alt}
+              onWheel={handleScroll}
+              style={{ transform: `scale(${zoomLevel})` }}
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
