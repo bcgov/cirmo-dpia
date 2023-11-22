954e7850792f800948082d764f0dbacfd628d4f3
Noor Chasib <noorchasib@gmail.com>
2023-11-22 14:33:26 -0800

    make pages only show 10 at a time and double chevrons to jump pages

--- src/frontend/src/components/common/Pagination/listPageNumber.tsx
+++ src/frontend/src/components/common/Pagination/listPageNumber.tsx
@@ -1,9 +1,50 @@
 import { ListPageNumberProps } from './helpers/interfaces';
 import { PaginationDirection } from './helpers/enums';
 import PaginationButton from './button';
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
-
+import {
+  faAngleLeft,
+  faAngleRight,
+  faAnglesLeft,
+  faAnglesRight,
+} from '@fortawesome/free-solid-svg-icons';
 const ListPageNumber = (props: ListPageNumberProps) => {
+  const totalPages = Math.ceil(props.totalEntries / props.pageSize);
+  const pageNumbersToShow = 10;
+
+  const getPaginationRange = () => {
+    const halfRange = Math.floor(pageNumbersToShow / 2);
+    let start = Math.max(1, props.currentPage - halfRange);
+    const end = Math.min(totalPages, start + pageNumbersToShow - 1);
+
+    if (end - start + 1 < pageNumbersToShow) {
+      start = Math.max(1, end - pageNumbersToShow + 1);
+    }
+
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  };
+
+  const handleLeftPageChange = () => {
+    props.changePage(Math.max(1, props.currentPage - 1));
+  };
+
+  const handleRightPageChange = () => {
+    props.changePage(Math.min(totalPages, props.currentPage + 1));
+  };
+
+  const handleFirstPageChange = () => {
+    props.changePage(1);
+  };
+
+  const handleLastPageChange = () => {
+    props.changePage(totalPages);
+  };
+
+  const handlePageNumberClick = (pageNumber: number) => {
+    props.changePage(pageNumber);
+  };
+
+  const paginationRange = getPaginationRange();
+
   return (
     <nav role="navigation" aria-label="Pagination Navigation">
       <ul className="page__enumeration__container">
@@ -11,43 +52,46 @@ const ListPageNumber = (props: ListPageNumberProps) => {
           currentPage={props.currentPage}
           totalEntries={props.totalEntries}
           pageSize={props.pageSize}
-          changePage={props.changePage}
-          icon={faAngleLeft}
+          changePage={handleFirstPageChange}
+          icon={faAnglesLeft}
           direction={PaginationDirection.left}
         />
-        {props.totalEntries > props.pageSize ? (
-          Array.from(
-            Array(Math.ceil(props.totalEntries / props.pageSize)),
-            (e, i) => {
-              return (
-                <li
-                  tabIndex={0}
-                  aria-label={'Go to Page ' + (i + 1)}
-                  key={i}
-                  onClick={() => {
-                    props.changePage(i + 1);
-                  }}
-                  aria-current={i + 1 === props.currentPage}
-                  className={`page__enumeration ${
-                    i + 1 === props.currentPage ? 'pageActive' : ' '
-                  }`}
-                >
-                  {i + 1}
-                </li>
-              );
-            },
-          )
-        ) : (
-          <li tabIndex={0} aria-current="true" aria-label={'Goto Page 1'}>
-            {props.currentPage}
-          </li>
-        )}
         <PaginationButton
           currentPage={props.currentPage}
           totalEntries={props.totalEntries}
           pageSize={props.pageSize}
-          changePage={props.changePage}
+          changePage={handleLeftPageChange}
+          icon={faAngleLeft}
+          direction={PaginationDirection.left}
+        />
+        {paginationRange.map((pageNumber) => (
+          <li
+            tabIndex={0}
+            aria-label={'Go to Page ' + pageNumber}
+            key={pageNumber}
+            onClick={() => handlePageNumberClick(pageNumber)}
+            aria-current={pageNumber === props.currentPage}
+            className={`page__enumeration ${
+              pageNumber === props.currentPage ? 'pageActive' : ''
+            }`}
+          >
+            {pageNumber}
+          </li>
+        ))}
+        <PaginationButton
+          currentPage={props.currentPage}
+          totalEntries={props.totalEntries}
+          pageSize={props.pageSize}
+          changePage={handleRightPageChange}
           icon={faAngleRight}
+          direction={PaginationDirection.right}
+        />
+        <PaginationButton
+          currentPage={props.currentPage}
+          totalEntries={props.totalEntries}
+          pageSize={props.pageSize}
+          changePage={handleLastPageChange}
+          icon={faAnglesRight}
           direction={PaginationDirection.right}
         />
       </ul>
