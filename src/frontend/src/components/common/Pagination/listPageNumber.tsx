b99426bcd10e3709159c8d60a7bed5ff67174429
Noor Chasib <noorchasib@gmail.com>
2023-11-23 10:17:11 -0800

    add comments

--- src/frontend/src/components/common/Pagination/listPageNumber.tsx
+++ src/frontend/src/components/common/Pagination/listPageNumber.tsx
@@ -8,21 +8,27 @@ import {
   faAnglesRight,
 } from '@fortawesome/free-solid-svg-icons';
 const ListPageNumber = (props: ListPageNumberProps) => {
+  // Calculate the total number of pages
   const totalPages = Math.ceil(props.totalEntries / props.pageSize);
+  // Define the number of page numbers to show at once
   const pageNumbersToShow = 10;
 
+  // Function to get the range of page numbers to display
   const getPaginationRange = () => {
     const halfRange = Math.floor(pageNumbersToShow / 2);
     let start = Math.max(1, props.currentPage - halfRange);
     const end = Math.min(totalPages, start + pageNumbersToShow - 1);
 
+    // Adjust the start and end to ensure the range length is consistent
     if (end - start + 1 < pageNumbersToShow) {
       start = Math.max(1, end - pageNumbersToShow + 1);
     }
 
+    // Create an array from the start to end page numbers
     return Array.from({ length: end - start + 1 }, (_, i) => start + i);
   };
 
+  // Handlers for page changes
   const handleLeftPageChange = () => {
     props.changePage(Math.max(1, props.currentPage - 1));
   };
@@ -32,20 +38,22 @@ const ListPageNumber = (props: ListPageNumberProps) => {
   };
 
   const handleFirstPageChange = () => {
-    props.changePage(1);
+    props.changePage(1); // Go to the first page
   };
 
   const handleLastPageChange = () => {
-    props.changePage(totalPages);
+    props.changePage(totalPages); // Go to the last page
   };
 
+  // Handler for clicking on a specific page number
   const handlePageNumberClick = (pageNumber: number) => {
     props.changePage(pageNumber);
   };
 
+  // Key down handler for keyboard navigation
   const handleKeyDown = (e: React.KeyboardEvent, pageNumber: number) => {
     if (e.key === 'Enter') {
-      handlePageNumberClick(pageNumber);
+      handlePageNumberClick(pageNumber); // Handle 'Enter' key as click
     }
   };
 
@@ -54,6 +62,7 @@ const ListPageNumber = (props: ListPageNumberProps) => {
   return (
     <nav role="navigation" aria-label="Pagination Navigation">
       <ul className="page__enumeration__container">
+        {/* Buttons for navigating to the first and previous pages */}
         <PaginationButton
           currentPage={props.currentPage}
           totalEntries={props.totalEntries}
@@ -70,6 +79,7 @@ const ListPageNumber = (props: ListPageNumberProps) => {
           icon={faAngleLeft}
           direction={PaginationDirection.left}
         />
+        {/* Page number buttons */}
         {paginationRange.map((pageNumber) => (
           <li
             tabIndex={0}
@@ -85,6 +95,7 @@ const ListPageNumber = (props: ListPageNumberProps) => {
             {pageNumber}
           </li>
         ))}
+        {/* Buttons for navigating to the next and last pages */}
         <PaginationButton
           currentPage={props.currentPage}
           totalEntries={props.totalEntries}
