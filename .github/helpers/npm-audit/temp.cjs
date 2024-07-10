f737788cc481e1283657695f559b8176826c565d
BradyMitch <braden.jr.mitch@gmail.com>
2024-07-10 10:26:31 -0700

    Test

--- .github/helpers/npm-audit/temp.cjs
+++ .github/helpers/npm-audit/temp.cjs
@@ -2,5 +2,5 @@ const path = require("path");
 const directoryPaths = JSON.parse(process.env.directoryPaths);
 
 directoryPaths.map((dirPath) => {
-  console.log(path.resolve(__dirname, `../../../${directoryPath}`));
+  console.log(path.resolve(__dirname, `../../../${dirPath}`));
 });
